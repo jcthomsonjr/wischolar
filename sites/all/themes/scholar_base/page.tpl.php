dac8cdc868953c0eac42073e3fa52fd917ecff9f
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-14 19:28:38 +0000

    minor changes

--- sites/all/themes/scholar_base/page.tpl.php
+++ sites/all/themes/scholar_base/page.tpl.php
@@ -12,102 +12,99 @@
 </head>
 <body class="<?php print $body_classes; ?>">
   <div id="wrapper">
-		<div id="container">		
-			<?php if ($header_top || $header_main || $header_left || $header_right || $navbar) { ?>
-			<div id="header">
-				<?php if (!empty($header_top)){ ?>
-				<div id="header-top">
-					<?php print $header_top; ?>
-				</div><!-- /header-top -->
-				<?php }; ?>
-				<div id="header-wrapper">
-                    <?php if (!empty($header_main)){ ?>
-                    <div id="header-main" class="column">
-                        <?php print $header_main; ?>
-                    </div><!-- /header-main -->
-                    <?php }; ?>
+    <div id="container">    
+      <?php if ($header_top || $header_main || $header_left || $header_right || $navbar) { ?>
+      <div id="header">
+        <?php if (!empty($header_top)){ ?>
+        <div id="header-top">
+          <?php print $header_top; ?>
+        </div><!-- /header-top -->
+        <?php }; ?>
+        <div id="header-wrapper">
+          <?php if (!empty($header_main)){ ?>
+            <div id="header-main" class="column">
+              <?php print $header_main; ?>
+            </div><!-- /header-main -->
+          <?php }; ?>
                     
-                    <?php if (!empty($header_left)){ ?>
-                    <div id="header-left" class="column">
-                        <?php print $header_left; ?>
-                    </div><!-- /header-left -->
-                    <?php }; ?>
+          <?php if (!empty($header_left)){ ?>
+            <div id="header-left" class="column">
+              <?php print $header_left; ?>
+            </div><!-- /header-left -->
+          <?php }; ?>
                     
-                    <?php if (!empty($header_right)){ ?>
-                    <div id="header-right" class="column">
-                        <?php print $header_right; ?>
-                    </div><!-- /header-right -->
-                    <?php }; ?>
-				</div><!-- /header wrapper -->				
-				<?php if (!empty($navbar)){ ?>
-				<div id="navbar">
-					<?php print $navbar; ?>
-				</div><!-- /navbar -->
-				<?php }; ?>				
-			</div> <!-- /header -->
-			<?php }; ?>	
-			
-			<div id="content-wrapper">
-			
-				<div id="content-main" class="column">
-					<?php if (!empty($content_top)){ ?>
-				  	<div id="content-top">
-				  		<?php print $content_top; ?>
-				  	</div><!-- /content-top -->
-					<?php }; ?>
-					
-					<div id="content">
-					<?php if (!empty($title)){ ?>
-						<h2 class="title"><?php print $title; ?></h2>
-					 <?php }; ?>
-					 <?php if (!empty($tabs)){ ?>
-						<div class="tabs"><?php print $tabs; ?></div>
-					  <?php }; ?>
-					  <?php print $help; ?>
-					  <?php print $messages; ?>
-					  <?php print $content; ?>
-					</div> <!-- /content -->
-				
-					<?php if (!empty($content_bottom)){ ?>
-				  	<div id="content-bottom">
-				  		<?php print $content_bottom; ?>
-				  	</div><!--/content-bottom-->
-					<?php }; ?>
-			  	</div><!-- /content main -->
-		        
-		      
-		        <?php if (!empty($left)){ ?>
-				<div id="sidebar-left" class="column">
-				  <?php print $left; ?>
-				</div> <!-- /sidebar-left -->
-				<?php }; ?>
-		
-			  	<?php if (!empty($right)){ ?>
-				<div id="sidebar-right" class="column">
-					<?php print $right; ?>
-				</div> <!-- /sidebar-right -->
-			  <?php }; ?>
-			  <
-			</div> <!-- / content wrapper -->
-			<div id="content-wrapper-bottom"></div>
-			<?php if ($footer || $footer_message) { ?>		
-		  <div id="footer">
-			<?php if ($footer_message){ ?>
-				<div id="footer-message"><?php print $footer_message; ?></div>
-			<?php }; ?>
-			<?php if ($footer){ ?>
-				<?php print $footer; ?>
-			<?php }; ?>
-		  </div> <!-- /#footer -->
-		  <?php }; ?>
-		</div> <!-- /container -->
-	</div> <!-- /wrapper -->
-	<div id="extradiv"></div>
+          <?php if (!empty($header_right)){ ?>
+          <div id="header-right" class="column">
+             <?php print $header_right; ?>
+          </div><!-- /header-right -->
+            <?php }; ?>
+        </div><!-- /header wrapper -->        
+        <?php if (!empty($navbar)){ ?>
+        <div id="navbar">
+          <?php print $navbar; ?>
+        </div><!-- /navbar -->
+        <?php }; ?>        
+      </div> <!-- /header -->
+      <?php }; ?>  
+      
+      <div id="content-wrapper">
+      
+        <div id="content-main" class="column">
+          <?php if (!empty($content_top)){ ?>
+            <div id="content-top">
+              <?php print $content_top; ?>
+            </div><!-- /content-top -->
+          <?php }; ?>
+          
+          <div id="content">
+          <?php if (!empty($title)){ ?>
+            <h2 class="title"><?php print $title; ?></h2>
+           <?php }; ?>
+           <?php if (!empty($tabs)){ ?>
+            <div class="tabs"><?php print $tabs; ?></div>
+            <?php }; ?>
+            <?php print $help; ?>
+            <?php print $messages; ?>
+            <?php print $content; ?>
+          </div> <!-- /content -->
+        
+          <?php if (!empty($content_bottom)){ ?>
+            <div id="content-bottom">
+              <?php print $content_bottom; ?>
+            </div><!--/content-bottom-->
+          <?php }; ?>
+          </div><!-- /content main -->
+            
+          
+            <?php if (!empty($left)){ ?>
+        <div id="sidebar-left" class="column">
+          <?php print $left; ?>
+        </div> <!-- /sidebar-left -->
+        <?php }; ?>
+    
+          <?php if (!empty($right)){ ?>
+        <div id="sidebar-right" class="column">
+          <?php print $right; ?>
+        </div> <!-- /sidebar-right -->
+        <?php }; ?>
+      </div> <!-- / content wrapper -->
+      <div id="content-wrapper-bottom"></div>
+      <?php if ($footer || $footer_message) { ?>    
+      <div id="footer">
+      <?php if ($footer_message){ ?>
+        <div id="footer-message"><?php print $footer_message; ?></div>
+      <?php }; ?>
+      <?php if ($footer){ ?>
+        <?php print $footer; ?>
+      <?php }; ?>
+      </div> <!-- /#footer -->
+      <?php }; ?>
+    </div> <!-- /container -->
+  </div> <!-- /wrapper -->
+  <div id="extradiv"></div>
   <?php if ($closure_region){ ?>
     <div id="closure-blocks"><?php print $closure_region; ?></div>
   <?php }; ?>
-
   <?php print $closure; ?>
-
 </body>
 </html>
