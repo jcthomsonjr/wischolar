f49380cee55e05be5d061e2aa476daf5849fdbcf
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-20 19:05:22 +0000

    new scholar layout module

---
+++ sites/scholar/modules/scholar_modules/scholarlayout/theme/scholarlayout-layout.tpl.php
@@ -0,0 +1,39 @@
+<div id="wrapper">
+	<div id="scholarlayout-container">
+		<div id="scholarlayout-header">
+			<div id="scholarlayout-header-wrapper">
+  			<div id="scholarlayout-header-main">
+  				<?php print $scholarlayout_header_main; ?>
+  			</div>
+        <div id="scholarlayout-header-left">
+        <?php print $scholarlayout_header_left; ?>
+        </div>
+        <div id="scholarlayout-header-right">
+        <?php print $scholarlayout_header_right; ?>
+        </div>
+			</div>
+			
+      <div id="scholarlayout-navbar">
+        <?php print $scholarlayout_navbar; ?>
+    	</div>
+  	</div> <!-- end of schlarlayout-header -->
+
+		<div id="scholarlayout-content-wrapper">
+			<div id="scholarlayout-content-main">
+			</div>
+			<div id="scholarlayout-sidebar-left">
+        <?php print $scholarlayout_left; ?>
+			</div>
+			<div id="scholarlayout-sidebar-right">
+			  <?php print $scholarlayout_right; ?>
+			</div>
+		</div>
+		
+		<div id="scholarlayout-content-wrapper-bottom">
+		</div>
+		
+		<div id="scholarlayout-footer">
+
+		</div>
+	</div>
+</div>
