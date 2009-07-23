abcc55ddfd7c97c60c376c8f88b7e37cbe6e49c4
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-23 18:52:08 +0000

    added googlescholar meta tags function

--- sites/all/themes/scholar_base/page.tpl.php
+++ sites/all/themes/scholar_base/page.tpl.php
@@ -11,7 +11,6 @@
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $body_classes; ?>">
-  <?php print $scholarcp_toolbar;?>
   <div id="wrapper">
     <div id="container">    
       <?php if ($header_top || $header_main || $header_left || $header_right || $navbar) { ?>
