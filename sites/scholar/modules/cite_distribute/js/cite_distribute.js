367248a6e48e4187d9ad61e80ba4241e20ebea97
jjweiner <jjweiner@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-24 20:33:59 +0000

    modified widget form

--- sites/scholar/modules/cite_distribute/js/cite_distribute.js
+++ sites/scholar/modules/cite_distribute/js/cite_distribute.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.citedistribute = function (context){
   
-  $('form#cite-distribute-form').hide();	
+  //$('form#cite-distribute-form').hide();	
   
   $('a#cite-distribute-link-title', context).click(function(){
 		$('form#cite-distribute-form').slideDown('slow');	
