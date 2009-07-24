b965818689f2030122eed3199bb6ebb7f5708a6f
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-24 17:18:16 +0000

    theming top menu

--- sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-toolbar.tpl.php
+++ sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-toolbar.tpl.php
@@ -1,14 +1,12 @@
-<div id='scholar-toolbar'>
-  <?php if (!empty($tree_0)): ?>
-      <?php foreach ($tree_0 as $links): ?>
-        <?php print $links; ?>
-      <?php endforeach; ?>
-   <?php endif; ?>
+<?php if (!empty($tree_0)): ?>
+    <?php foreach ($tree_0 as $links): ?>
+      <?php print $links; ?>
+    <?php endforeach; ?>
+ <?php endif; ?>
 
-  <?php if (!empty($tree_1)): ?>
-      <span class='close'><?php print t('Close') ?></span>
-      <?php foreach ($tree_1 as $links): ?>
-        <?php print $links; ?>
-      <?php endforeach; ?>
-  <?php endif; ?>
-</div>
+<?php if (!empty($tree_1)): ?>
+    <span class='close'><?php print t('Close') ?></span>
+    <?php foreach ($tree_1 as $links): ?>
+      <?php print $links; ?>
+    <?php endforeach; ?>
+<?php endif; ?>
