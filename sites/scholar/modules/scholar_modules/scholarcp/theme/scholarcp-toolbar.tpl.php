24245565f8ed2fb04aee4f9786db9a6cc365e74e
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-24 17:17:19 +0000

    theming top menu

--- sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-toolbar.tpl.php
+++ sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-toolbar.tpl.php
@@ -1,20 +1,14 @@
-<div id='scholar-toolbar' class='clear-block'>
+<div id='scholar-toolbar'>
   <?php if (!empty($tree_0)): ?>
-    <div class='depth-0 clear-block'>
       <?php foreach ($tree_0 as $links): ?>
         <?php print $links; ?>
       <?php endforeach; ?>
-    </div>
-  <?php endif; ?>
+   <?php endif; ?>
 
   <?php if (!empty($tree_1)): ?>
-    <div class='depth-1 clear-block <?php if ($collapsed) print 'collapsed' ?>'>
       <span class='close'><?php print t('Close') ?></span>
       <?php foreach ($tree_1 as $links): ?>
         <?php print $links; ?>
       <?php endforeach; ?>
-    </div>
   <?php endif; ?>
-
-  <div class='shadow'></div>
 </div>
