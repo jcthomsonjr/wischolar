a2252a844a52baaa909a30b1c0c72ee99d966141
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-27 14:32:37 +0000

    template file for the theme picker

---
+++ sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-theme-picker.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * template file for theming the theme_picker
+ * Note that this is used just for the img, theme name and theme description
+ * and not the html radios
+ * Variables:
+ * ----------
+ * $info : the whole theme object  (dpm($info) to see everything
+ * 
+ */
+
+  $screenshot = t('no screenshoot available');
+  if (file_exists($info['screenshot'])){
+    $screenshot = theme('image', $info['screenshot'], t('Screenshot for %theme theme', array('%theme' => $info['theme_name'])), '', array('class' => 'screenshot'), FALSE);
+  }
+  print $screenshot . $info['theme_name'] . $info['description'];
+?>
