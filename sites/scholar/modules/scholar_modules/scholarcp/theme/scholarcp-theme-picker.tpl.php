ca14e7b7a1c7e3c0a2a5b7364c8629b542542e66
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-28 14:00:13 +0000

    various minor changes

--- sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-theme-picker.tpl.php
+++ sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-theme-picker.tpl.php
@@ -8,10 +8,11 @@
  * $info : the whole theme object  (dpm($info) to see everything
  * 
  */
-
+  
+  
   $screenshot = t('no screenshoot available');
   if (file_exists($info['screenshot'])){
-    $screenshot = theme('image', $info['screenshot'], t('Screenshot for %theme theme', array('%theme' => $info['theme_name'])), '', array('class' => 'screenshot'), FALSE);
+    $screenshot = theme('image', $info['screenshot'], t('Screenshot for %theme theme', array('%theme' => $info['name'])), '', array('class' => 'screenshot'), FALSE);
   }
-  print '<div class="item-theme-picker"><div class="screenshot">'. $screenshot .'</div><h3>' . $info['theme_name'] . '</h3><p>'. $info['description'] .'</p></div>';
+  print '<div class="item-theme-picker"><div class="screenshot">'. $screenshot .'</div><h3>"' . $info['name'] . '"</h3><p>'. $info['description'] .'</p></div>';
 ?>
