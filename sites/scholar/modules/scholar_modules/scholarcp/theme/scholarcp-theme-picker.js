a385e19dbf0252ed999c65afb2f3c3a4a8806ecf
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-28 16:08:22 +0000

    theme selection through javascript

--- sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-theme-picker.js
+++ sites/scholar/modules/scholar_modules/scholarcp/theme/scholarcp-theme-picker.js
@@ -1,15 +1,16 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function() {
-    $('#scholarcp-settings-theme #scholarcp-settings-form ').hide();
-    
-    var theList = '<ul class = "theme-picker">';
+    $('#scholarcp-settings-theme .form-radios ').hide();
+
+    var theList = '<ul class = "theme-picker">'; // important, RSC depend on it
     
     function getLi(liId, liChecked, liContent){
     	return '<li class = "item-theme-picker  ' + liChecked + '" id="' + liId  + '">' + liContent + '</li>';
     	
     }
     $('#scholarcp-settings-form .form-radios .form-item').each(function(){
-    	var liId = $(this).attr('id');
+    	var liIdArr = $(this).attr('id').split('-');
+    	var liId = liIdArr[3] + '-' + liIdArr[4] + '-' + liIdArr[5];
     	var liChecked = $(this).find('input').attr('checked') ? 'checked' : '';
 
     	// get the whole img div
@@ -28,6 +29,17 @@ if (Drupal.jsEnabled) {
     
     
     $('div#prev, div#next').RSC();
+    
+    $('li.item-theme-picker').click(function(){
+    	// remove the active class from every li first
+    	$(".item-theme-picker").removeClass('checked');
+    	// add the class to this one
+    	$(this).addClass('checked');
+    	
+    	var id = $(this).attr('id');
+    	$(".form-radio").removeAttr("checked");
+    	$("#edit-settings-theme-" + id).attr("checked", "checked"); 
+    });
     
   });
 }
