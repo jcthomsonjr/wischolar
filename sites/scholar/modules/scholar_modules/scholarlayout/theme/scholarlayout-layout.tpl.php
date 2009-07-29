57ceb3fcb9bad8460a7e693c843ef8e92ae46602
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-29 15:02:13 +0000

    adding initial theming to layout module

--- sites/scholar/modules/scholar_modules/scholarlayout/theme/scholarlayout-layout.tpl.php
+++ sites/scholar/modules/scholar_modules/scholarlayout/theme/scholarlayout-layout.tpl.php
@@ -2,29 +2,29 @@
 	<div id="scholarlayout-container">
 		<div id="scholarlayout-header">
 			<div id="scholarlayout-header-wrapper">
-  			<div id="scholarlayout-header-main">
+  			<div id="scholarlayout-header-main" class="layout-region">
   				<?php print $scholarlayout_header_main; ?>
   			</div>
-        <div id="scholarlayout-header-left">
+        <div id="scholarlayout-header-left" class="layout-region">
         <?php print $scholarlayout_header_left; ?>
         </div>
-        <div id="scholarlayout-header-right">
+        <div id="scholarlayout-header-right" class="layout-region">
         <?php print $scholarlayout_header_right; ?>
         </div>
 			</div>
 			
-      <div id="scholarlayout-navbar">
+      <div id="scholarlayout-navbar" class="layout-region">
         <?php print $scholarlayout_navbar; ?>
     	</div>
   	</div> <!-- end of schlarlayout-header -->
 
 		<div id="scholarlayout-content-wrapper">
-			<div id="scholarlayout-content-main">
+			<div id="scholarlayout-content-main" class="layout-region">
 			</div>
-			<div id="scholarlayout-sidebar-left">
+			<div id="scholarlayout-sidebar-left" class="layout-region">
         <?php print $scholarlayout_left; ?>
 			</div>
-			<div id="scholarlayout-sidebar-right">
+			<div id="scholarlayout-sidebar-right" class="layout-region">
 			  <?php print $scholarlayout_right; ?>
 			</div>
 		</div>
@@ -32,7 +32,7 @@
 		<div id="scholarlayout-content-wrapper-bottom">
 		</div>
 		
-		<div id="scholarlayout-footer">
+		<div id="scholarlayout-footer" class="layout-region">
 
 		</div>
 	</div>
