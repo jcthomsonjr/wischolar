20d4774d70bc0652726b64cd2f0baa93a6ed49f5
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-29 19:50:43 +0000

    normalized class name of widget

--- sites/scholar/modules/scholar_modules/scholarlayout/theme/scholarlayout-layout-top.tpl.php
+++ sites/scholar/modules/scholar_modules/scholarlayout/theme/scholarlayout-layout-top.tpl.php
@@ -9,7 +9,7 @@
 <div id="scholarlayout-top-widgets">
 	<ul class = "scholarlayout-top-widgets-list">
 	<?php foreach($wgts as $w):?>
-		<li class="scholarlayout-top-item" id="<?php print $w['module']; ?>-<?php print $w['delta']; ?>"> <?php print $w['label']; ?> </li>
+		<li class="scholarlayout-item" id="<?php print $w['module']; ?>-<?php print $w['delta']; ?>"> <?php print $w['label']; ?> </li>
 	<?php endforeach?>
 	</ul>
 </div>
