b2ebcee6c068d0728815b1c3eb059e8c586942ce
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-07-30 15:52:25 +0000

    removed divs on layout map

--- sites/scholar/modules/scholar_modules/scholarlayout/theme/scholarlayout-layout.tpl.php
+++ sites/scholar/modules/scholar_modules/scholarlayout/theme/scholarlayout-layout.tpl.php
@@ -1,39 +1,8 @@
-<div id="wrapper">
-	<div id="scholarlayout-container" class="pad">
-		<div id="scholarlayout-header">
-			<div id="scholarlayout-header-wrapper">
-  			<div id="scholarlayout-header-main" class="layout-region">
-  				<?php print $scholarlayout_header_main; ?>
-  			</div>
-        <div id="scholarlayout-header-left" class="layout-region">
-        <?php print $scholarlayout_header_left; ?>
-        </div>
-        <div id="scholarlayout-header-right" class="layout-region">
-        <?php print $scholarlayout_header_right; ?>
-        </div>
-			</div>
-			
-      <div id="scholarlayout-navbar" class="layout-region">
-        <?php print $scholarlayout_navbar; ?>
-    	</div>
-  	</div> <!-- end of schlarlayout-header -->
-
-		<div id="scholarlayout-content-wrapper">
-			<div id="scholarlayout-content-main" class="layout-region">
-			</div>
-			<div id="scholarlayout-sidebar-left" class="layout-region">
-        <?php print $scholarlayout_left; ?>
-			</div>
-			<div id="scholarlayout-sidebar-right" class="layout-region">
-			  <?php print $scholarlayout_right; ?>
-			</div>
-		</div>
-		
-		<div id="scholarlayout-content-wrapper-bottom">
-		</div>
-		
-		<div id="scholarlayout-footer" class="layout-region">
-
-		</div>
-	</div>
+<div id="scholarlayout-container" class="pad">
+  <?php print $scholarlayout_header_main; ?>
+  <?php print $scholarlayout_header_left; ?>
+  <?php print $scholarlayout_header_right; ?>
+  <?php print $scholarlayout_navbar; ?>
+  <?php print $scholarlayout_left; ?>
+  <?php print $scholarlayout_right; ?>
 </div>
