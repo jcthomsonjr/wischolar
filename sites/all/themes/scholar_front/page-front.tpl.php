43f49389353046617b6e216b9dd8e4aca0e31939
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-05 20:18:11 +0000

    changed of theme to scholar project

--- sites/all/themes/scholar_front/page-front.tpl.php
+++ sites/all/themes/scholar_front/page-front.tpl.php
@@ -72,42 +72,42 @@
               <div id="new-sites">
                 <div class="new-site-block">
                   <div class="photo">
-                    <img src="<?php print $base_url . $directory;?>/images/scholar-photo1.jpg" />
+                    <a href="site01"><img src="<?php print $base_url . $directory;?>/images/scholar-photo1.jpg" /></a>
                    </div>
                  <h4>Susan J. Pharr</h4>
                  <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
                 </div>
                 <div class="new-site-block">
                   <div class="photo">
-                    <img src="<?php print $base_url . $directory;?>/images/scholar-photo2.jpg" />
+                    <a href="site02"><img src="<?php print $base_url . $directory;?>/images/scholar-photo2.jpg" /></a>
                    </div>
                  <h4>Susan J. Pharr</h4>
                  <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
                 </div>
                 <div class="new-site-block">
                   <div class="photo">
-                    <img src="<?php print $base_url . $directory;?>/images/scholar-photo3.jpg" />
+                    <a href="site03"><img src="<?php print $base_url . $directory;?>/images/scholar-photo3.jpg" /></a>
                    </div>
                  <h4>Susan J. Pharr</h4>
                  <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
                 </div>
                 <div class="new-site-block">
                   <div class="photo">
-                    <img src="<?php print $base_url . $directory;?>/images/scholar-photo4.jpg" />
+                    <a href="site04"><img src="<?php print $base_url . $directory;?>/images/scholar-photo4.jpg" /></a>
                    </div>
                  <h4>Susan J. Pharr</h4>
                  <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
                 </div>
                 <div class="new-site-block">
                   <div class="photo">
-                    <img src="<?php print $base_url . $directory;?>/images/scholar-photo5.jpg" />
+                    <a href="site05"><img src="<?php print $base_url . $directory;?>/images/scholar-photo5.jpg" /></a>
                    </div>
                  <h4>Susan J. Pharr</h4>
                  <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
                 </div>
                 <div class="new-site-block">
                   <div class="photo">
-                    <img src="<?php print $base_url . $directory;?>/images/scholar-photo6.jpg" />
+                    <a href="site06"><img src="<?php print $base_url . $directory;?>/images/scholar-photo6.jpg" /></a>
                    </div>
                  <h4>Susan J. Pharr</h4>
                  <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
