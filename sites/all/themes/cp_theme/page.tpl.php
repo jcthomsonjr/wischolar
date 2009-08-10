a06dea956d207c7237fdfbcf2e749588f8f6661a
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-10 17:25:09 +0000

    trying re-commit of cp theme

---
+++ sites/all/themes/cp_theme/page.tpl.php
@@ -0,0 +1,67 @@
+<?php
+// $Id: page.tpl.php,v 1.10.2.4 2009/02/13 17:30:22 swortis Exp $
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $body_classes; ?>">
+  <div id="container">
+  	 <div id="top">
+  	   <?php print $scholarcp_toolbar;?>
+  	 </div>
+     <div id="main-content">
+      <div id="cp-sidemenu">
+        <div class="frame">
+          <div class="innerframe">
+          <?php print $scholarcp_left;?>
+          </div>
+        </div>
+			</div><!-- /cp-sidemenu -->
+			<div id="cp-content">
+			  <div class="frame">
+          <div class="innerframe">
+            <?php if (!empty($tabs)){ ?>
+            <div class="tabs"><?php print $tabs; ?></div>
+            <?php }; ?>
+            <?php print $help; ?>
+            <?php print $messages; ?>
+            <?php print $content; ?>
+            <?php if (!empty($feed_icons)){ ?>
+            <div class="feed-icons"><?php print $feed_icons; ?></div>
+            <?php }; ?>
+			    </div>
+        </div>
+			</div> <!-- /cp-content -->
+		</div> <!-- / main-content -->
+
+      <div id="footer">
+        <h3>The Scholars' Web Sites Project <span>IQSS, Harvard University</span></h3>
+        <ul> 
+          <li>&copy; 2008-<?php echo date(Y);?> President &amp; Fellows Harvard University</li>
+          <li><a href="/docs">Documentation</a></li>
+          <li><a href="/contact">Feedback</a></li>
+          <li><a href="/legal">Legal</a></li>
+          <li class="last">Version 2.13</li>
+        </ul>
+        <?php if ($footer_message){ ?>
+          	<div id="footer-message"><?php print $footer_message; ?></div>
+        <?php }; ?>
+        <?php if ($footer){ ?>
+        	<?php print $footer; ?>
+        <?php }; ?>
+      </div> <!-- /#footer -->
+	</div> <!-- /container --> 
+  <?php if ($closure_region){ ?>
+    <div id="closure-blocks"><?php print $closure_region; ?></div>
+  <?php }; ?>
+
+  <?php print $closure; ?>
+    </div> <!-- /container -->  
+</body>
+</html>
