e92c00c458c91cfa635dc113a218164c1e5d8f98
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-14 14:12:37 +0000

    Adding jquerylink module - for themable submit buttons

---
+++ sites/scholar/modules/scholar_modules/jquerylink/jquery.link.js
@@ -0,0 +1,20 @@
+﻿$(function()
+  {
+    $(':submit').each(
+       function()
+       {
+          var $this = $(this);
+          var f = this.form;
+          var link = $('<a href="#"><span>' + $this.val() + '</span></a>')
+             .bind(
+                'click',
+                function()
+                {
+                   $(f).trigger('submit');
+                   return false;
+                }
+             )
+          $this.after(link).css({position:'absolute', top:'-2000px'});
+       }
+    )
+});
