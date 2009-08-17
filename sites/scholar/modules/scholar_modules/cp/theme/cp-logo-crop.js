fa3dfd643a2d80c33e435d5a0bd691bf3138922e
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-17 14:37:24 +0000

    remove preview

--- sites/scholar/modules/scholar_modules/cp/theme/cp-logo-crop.js
+++ sites/scholar/modules/scholar_modules/cp/theme/cp-logo-crop.js
@@ -1,7 +1,7 @@
 
-function updateLogo(c){
+function cp_updateLogo(c){
 		$('#edit-settings-logo-x-cord').val(c.x);
 		$('#edit-settings-logo-y-cord').val(c.y);
 		$('#edit-settings-logo-width').val(c.w);
-		$('#edit-settings-logo-height').val(c.h);	
+		$('#edit-settings-logo-height').val(c.h);
 }
