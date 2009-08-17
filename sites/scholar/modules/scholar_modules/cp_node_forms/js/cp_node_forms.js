23021949d1e6a08bfa3520a167cd70bf66050b23
jjweiner <jjweiner@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-17 18:17:08 +0000

    fixed js for publish on another date textfield

--- sites/scholar/modules/scholar_modules/cp_node_forms/js/cp_node_forms.js
+++ sites/scholar/modules/scholar_modules/cp_node_forms/js/cp_node_forms.js
@@ -1,18 +1,19 @@
 Drupal.behaviors.cp_node_forms = function (context){
-  
-  $('div#edit-pubdate-wrapper').hide();	
-  
+
   $('input#edit-pubstatus-pubondate', context).click(function(){
 		$('div#edit-pubdate-wrapper').fadeIn('fast');	
+		 $('div#pubdate').removeClass('hide-pubdate');  
   });
 
 	  $('input#edit-pubstatus-save', context).click(function(){
 		$('div#edit-pubdate-wrapper').fadeOut('fast');
+		 $('div#pubdate').addClass('hide-pubdate');  
 			
 	});
 	
 	  $('input#edit-pubstatus-publish', context).click(function(){
 		$('div#edit-pubdate-wrapper').fadeOut('fast');
+		 $('div#pubdate').addClass('hide-pubdate');  
 	});
 
 }
