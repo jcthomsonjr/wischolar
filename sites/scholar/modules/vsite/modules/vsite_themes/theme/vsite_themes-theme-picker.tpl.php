de0885985886e44ef471c8c0ad1972cabddbd2aa
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-21 16:29:27 +0000

    refactoring fix vite_themes

---
+++ sites/scholar/modules/vsite/modules/vsite_themes/theme/vsite_themes-theme-picker.tpl.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * template file for theming the theme_picker
+ * Note that this is used just for the img, theme name and theme description
+ * and not the html radios
+ * Variables:
+ * ----------
+ * $info : the whole theme object  (dpm($info) to see everything
+ * 
+ */
+  
+  
+  $screenshot = t('no screenshoot available');
+  if (file_exists($info['screenshot'])){
+    $screenshot = theme('image', $info['screenshot'], t('Screenshot for %theme theme', array('%theme' => $info['name'])), '', array('class' => 'screenshot'), FALSE);
+  }
+  print '<div class="item-theme-picker"><div>'. $screenshot .'</div><h3>"' . $info['name'] . '"</h3><p>'. $info['description'] .'</p></div>';
+?>
