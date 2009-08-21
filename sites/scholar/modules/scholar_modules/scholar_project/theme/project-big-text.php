31c3244ab3935d637518d37b40ac588e39df5504
jjweiner <jjweiner@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-21 19:40:24 +0000

    added html to tpl files

--- sites/scholar/modules/scholar_modules/scholar_project/theme/project-big-text.php
+++ sites/scholar/modules/scholar_modules/scholar_project/theme/project-big-text.php
@@ -1,5 +1,4 @@
   <div id="tutorial-link">See a video tutorial</div>
-  
   <div class="content">
     <h2>Create a dynamic feature-full web site in seconds. For free!</h2>
     <h3>(No, there’s no catch.)</h3>
