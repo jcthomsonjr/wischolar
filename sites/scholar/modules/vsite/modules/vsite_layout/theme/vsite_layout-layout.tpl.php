d7e8d2b8186bf1d25991bde196255807f12f0d40
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-29 18:13:36 +0000

    move vsite modules in sites/all/modules

--- sites/scholar/modules/vsite/modules/vsite_layout/theme/vsite_layout-layout.tpl.php
+++
@@ -1,9 +0,0 @@
-<?= $vsite_layout_form; ?>
-<div id="scholarlayout-container" class="pad">
-  <?php print $vsite_layout_header_main; ?>
-  <?php print $vsite_layout_header_left; ?>
-  <?php print $vsite_layout_header_right; ?>
-  <?php print $vsite_layout_navbar; ?>
-  <?php print $vsite_layout_left; ?>
-  <?php print $vsite_layout_right; ?>
-</div>
