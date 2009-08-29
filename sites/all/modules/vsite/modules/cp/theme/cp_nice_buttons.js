12fad284a8f27784eb6cdfbfd3b3548a6b956453
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-29 22:05:42 +0000

    moving nice-button.* under cp

---
+++ sites/all/modules/vsite/modules/cp/theme/cp_nice_buttons.js
@@ -0,0 +1,14 @@
+﻿Drupal.behaviors.CpNiceButton = function(context){
+  $(':submit:not(.cp-nice-button-processed)', context)
+  .addClass('cp-nice-button-processed')
+  .each(function(){
+    var $this = $(this);
+    var f = this.form;
+    var link = $('<a class="submit-link" href="#"><span class="button-inner"><span class="label">' + $this.val() + '</span></span></a>')
+               .bind('click',function(){
+                 $(f).trigger('submit');
+                 return false;
+               });
+    $this.after(link).css({position:'absolute', top:'-2000px'});	  
+  });
+}
