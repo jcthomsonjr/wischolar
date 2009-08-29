526fdc13460e6cbbba6b44973226035eb04b12d0
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-08-29 17:13:09 +0000

    the module creates its own content type

--- sites/scholar/modules/advanced_help2book/node-help.tpl.php
+++ sites/scholar/modules/advanced_help2book/node-help.tpl.php
@@ -1,4 +1,5 @@
-
+<?php //dpm(get_defined_vars());?>
 <?php //print $advanced_help2book_node_header; ?>
 <?php print $content; ?>
+<?php print 'xxxx'; ?>
 <?php //print $advanced_help2book_node_footer; ?>
