571593c2e417c8ca5b7a4c5978866d6e8aa8248b
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-09-02 18:19:05 +0000

    nice buttons

--- sites/all/modules/vsite/modules/cp/theme/cp_nice_buttons.js
+++ sites/all/modules/vsite/modules/cp/theme/cp_nice_buttons.js
@@ -3,10 +3,11 @@
   .addClass('cp-nice-button-processed')
   .each(function(){
     var $this = $(this);
-    var f = this.form;
+    //var f = this.form;
     var link = $('<a class="submit-link" href="#"><span class="button-inner"><span class="label">' + $this.val() + '</span></span></a>')
                .bind('click',function(){
-                 $(f).trigger('submit');
+                 //$(f).trigger('submit');
+            	 $this.click();
                  return false;
                });
     $this.after(link).css({position:'absolute', top:'-2000px'});	  
