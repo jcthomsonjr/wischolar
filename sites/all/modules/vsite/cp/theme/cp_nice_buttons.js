9ea9d971befe0f2604ad03c7877802f824f418f5
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-09-10 19:55:25 +0000

    scope fix

--- sites/all/modules/vsite/cp/theme/cp_nice_buttons.js
+++ sites/all/modules/vsite/cp/theme/cp_nice_buttons.js
@@ -5,16 +5,28 @@
     var $this = $(this);
     //var f = this.form;
     var link = $('<a class="submit-link ' + $this.attr("id") + '" href="#"><span class="button-inner"><span class="label">' + $this.val() + '</span></span></a>')
-               .bind('click',[$this],function(e){
-                 e.data[0].click();
+               .bind('click mousedown mouseup',[$this],function(e){
+            	 var elem = refresh_nicebutton_element(e.data[0]);
+            	 elem.click();
                  return false;
                }).bind('mousedown',[$this],function(e){
-            	 e.data[0].trigger('mousedown');
+            	 var elem = refresh_nicebutton_element(e.data[0]);
+            	 elem.trigger('mousedown');
                  return true;
                }).bind('mouseup',[$this],function(e){
-            	 e.data[0].trigger('mouseup');
+            	 var elem = refresh_nicebutton_element(e.data[0]);
+            	 elem.trigger('mouseup');
                  return true;
                });
     $this.after(link).css({position:'absolute', top:'-2000px'});	  
   });
+}
+
+function refresh_nicebutton_element(elem){
+	if(elem.attr("id")){
+		var tmp = $('#'+elem.attr("id"));
+		if(tmp.length) elem = tmp; 
+	}
+	
+	return elem;
 }
