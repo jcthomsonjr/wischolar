f8f32786098a8f5d47327ebba36d4d6e067272dd
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-09-14 20:12:39 +0000

    fix ctools

--- sites/all/themes/scholar_base/script.js
+++ sites/all/themes/scholar_base/script.js
@@ -21,36 +21,3 @@ Drupal.theme.CToolsModalDialog = function () {
 
   return html;
 }
-
-
-/**
- * AJAX responder command to place HTML within the modal.
- */
-Drupal.CTools.AJAX.commands.modal_display = function(command) {
-  $('#modal-title').html(command.title);
-  $('#modal-content').html(command.output);
-  var container = $('#modal-content');
-  var form = $('#modal-content > form');
-  if(container.length && form.length && container.height() < form.height()){
-	  if(form.find('fieldset').length == 1){
-		  var col = $("<div class='modal_col'></div>");
-		  var col_height = (form.height() > (container.height() * 2))?Math.round(form.height()/2):container.height();
-		  form.find('fieldset').prepend(col);
-		  form.find('fieldset .form-item').each(function(index,domElement) {
-			  var el = $(domElement);
-			  var tmp_h = $('.modal_col:last').height();
-			  var tmp_hc = container.height();
-			  if((el.height() + $('.modal_col:last').height()) > col_height && $('.modal_col').length < 2){
-				col = $("<div class='modal_col'></div>");
-			    $('.modal_col:last').after(col);
-			  }
-			  $('.modal_col:last').append(el);
-			  if($('.modal_col').length == 2 && $('.modal_col:last').height() > $('.modal_col:first').height()){
-				  $('.modal_col:first').append($('.modal_col:last .form-item:first'));
-			  }
-		  });
-		  
-	  }//Don't try this if there are a lot of fieldsets / or none
-  }
-  Drupal.attachBehaviors($('#modal-content'));
-}
