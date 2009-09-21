cf308c22c26620e345bfa96a5c8eb15bd3651c5a
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-09-21 19:35:41 +0000

    the list of sites dynamic

--- sites/all/modules/scholar/modules/scholar_project/theme/project-site-examples.tpl.php
+++ sites/all/modules/scholar/modules/scholar_project/theme/project-site-examples.tpl.php
@@ -1,46 +1,16 @@
-  <h3>New Sites</h3>
+<h3>New Sites</h3>
   <div id="search-person"></div>
   <div id="new-sites">
+
+<?php foreach($sites as $site):?>
   <div class="new-site-block">
     <div class="photo">
-      <a href="site01"><img src="<?php print $base_url . $directory;?>/images/scholar-photo1.jpg" /></a>
+      <?php print $site['photo'];?>
      </div>
-   <h4>Susan J. Pharr</h4>
-   <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
-  </div>
-  <div class="new-site-block">
-    <div class="photo">
-      <a href="site02"><img src="<?php print $base_url . $directory;?>/images/scholar-photo2.jpg" /></a>
-     </div>
-   <h4>Susan J. Pharr</h4>
-   <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
-  </div>
-  <div class="new-site-block">
-    <div class="photo">
-      <a href="site03"><img src="<?php print $base_url . $directory;?>/images/scholar-photo3.jpg" /></a>
-     </div>
-   <h4>Susan J. Pharr</h4>
-   <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
-  </div>
-  <div class="new-site-block">
-    <div class="photo">
-      <a href="site04"><img src="<?php print $base_url . $directory;?>/images/scholar-photo4.jpg" /></a>
-     </div>
-   <h4>Susan J. Pharr</h4>
-   <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
-  </div>
-  <div class="new-site-block">
-    <div class="photo">
-      <a href="site05"><img src="<?php print $base_url . $directory;?>/images/scholar-photo5.jpg" /></a>
-     </div>
-   <h4>Susan J. Pharr</h4>
-   <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
-  </div>
-  <div class="new-site-block">
-    <div class="photo">
-      <a href="site06"><img src="<?php print $base_url . $directory;?>/images/scholar-photo6.jpg" /></a>
-     </div>
-   <h4>Susan J. Pharr</h4>
-   <h5>Edwin O. Reischauer Professor of Japanese Politics</h5>
-  </div>
-  </div>
+   <h4><?php print $site['headline'];?></h4>
+   <h5><?php print $site['sub_headline'];?></h5>
+   </div>
+<?php endforeach;?>
+
+
+ 
