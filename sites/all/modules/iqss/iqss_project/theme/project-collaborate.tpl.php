01bb7025035885e6eab6f69b8195e52dbc1a9179
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-09-23 23:01:48 +0000

    rename scholar_project to iqss_project

---
+++ sites/all/modules/iqss/iqss_project/theme/project-collaborate.tpl.php
@@ -0,0 +1,4 @@
+<div id="feature-collaborate" class="featureblock">
+  <h3>Collaborate with your colleagues.</h3>
+  <p>Allow commentary on your work by whomever you select. Manage your Twtter feeds; keep track of colleagues’ publications and bl  ogs.</p>
+</div>
