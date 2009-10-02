d36e710b81ad013a5c9cf51c090eaaea7edcc660
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-10-02 03:06:32 +0000

    admin links

---
+++ sites/all/modules/scholar/theme/scholar-admin-links.tpl.php
@@ -0,0 +1,11 @@
+<?php if ($links): ?>
+
+<div class='scholar-scholar-admin-inline'>
+  <div class='scholar-admin-border scholar-admin-border-top'></div>
+  <div class='scholar-admin-border scholar-admin-border-right'></div>
+  <div class='scholar-admin-border scholar-admin-border-bottom'></div>
+  <div class='scholar-admin-border scholar-admin-border-left'></div>
+  <div class='scholar-admin-links clear-block'><?php print $links ?></div>
+</div>
+
+<?php endif; ?>
