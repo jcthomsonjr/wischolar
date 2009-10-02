d3bbbeb07400db9c8f585aa05e5bf907dd3f1c9d
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-10-02 17:40:59 +0000

    changed to slideToggle animation

--- sites/all/modules/scholar/theme/scholar.js
+++ sites/all/modules/scholar/theme/scholar.js
@@ -1,6 +1,6 @@
 Drupal.behaviors.scholar = function (context){
         $('.toggle', context).click(function(){
-                $(this).next('.slider').slideToggle(400);
+                $(this).next('.slider').slideToggle("fast");
                 return false;
         });
 }
