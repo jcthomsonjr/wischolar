02ab578e174604cc4f0eced74ed6afe88c4b700e
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-10-02 19:42:40 +0000

    class expanded when toggle is expanded

--- sites/all/modules/scholar/theme/scholar.js
+++ sites/all/modules/scholar/theme/scholar.js
@@ -1,6 +1,7 @@
 Drupal.behaviors.scholar = function (context){
-        $('.toggle', context).click(function(){
-                $(this).next('.slider').slideToggle("fast");
-                return false;
-        });
+  $('.toggle', context).click(function(){
+       $(this).next('.slider').slideToggle("fast").toggleClass("expanded");
+;
+       return false;
+  });
 }
