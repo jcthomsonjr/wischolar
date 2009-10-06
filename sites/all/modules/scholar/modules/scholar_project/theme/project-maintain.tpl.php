5fcc77caad8677ad652c26e7426c331cbe2703d6
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-10-06 18:22:33 +0000

    renamed to iqss_project

--- sites/all/modules/scholar/modules/scholar_project/theme/project-maintain.tpl.php
+++
@@ -1,5 +0,0 @@
-<div id="feature-biblio" class="featureblock">
-  <h3>Maintain an accurate and  citable bibliography of all your writings.</h3>
-  <p>Automatically have your publications submitted to induces such as RePEc, Computer Science Reposity, and Google Scholar.
-  </p>
-</div>
