d8666c505322140e863999c06ed1d363e31eb86c
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-10-13 16:34:23 +0000

    respond to Merce tickets

--- sites/all/modules/iqss/iqss_project/theme/project-collaborate.tpl.php
+++ sites/all/modules/iqss/iqss_project/theme/project-collaborate.tpl.php
@@ -1,4 +1,4 @@
 <div id="feature-collaborate" class="featureblock">
   <h3>Collaborate with your colleagues.</h3>
-  <p>Allow commentary on your work by whomever you select. Manage your Twtter feeds; keep track of colleagues’ publications and bl  ogs.</p>
+  <p>Allow commentary on your work by whomever you select. Manage your Twitter feeds; keep track of colleagues’ publications and blogs.</p>
 </div>
