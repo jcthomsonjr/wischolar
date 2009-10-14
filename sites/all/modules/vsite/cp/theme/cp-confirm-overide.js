f99ccfbd70e7ae5434b246da38d388e2dfe13080
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-10-14 18:13:10 +0000

    make bug fixes for user reg

--- sites/all/modules/vsite/cp/theme/cp-confirm-overide.js
+++ sites/all/modules/vsite/cp/theme/cp-confirm-overide.js
@@ -1,33 +1,30 @@
-if(Drupal.jsEnabled){
-	$(document).ready(function(){
+Drupal.behaviors.cp_confirm_overide_handler = function (context) {
 		var s_urls = "";
-		$("div#cp-content a").each(function(){
+		var s_container = $("div#cp-content").length ? "div#cp-content":"div.content";
+		$(s_container+" a:not(.cp_confirm_overide_handler-processed)").addClass('cp_confirm_overide_handler-processed').each(function(){
 			s_urls += "paths[]="+$(this).attr('href')+"&";
 		});
 
 		//Make call to check the urls to replace
-		$.ajax({
-		   type: "POST",
-		   url: location.pathname.substring(0,location.pathname.indexOf('/',1))+"/cp/allow_callback_override",
-		   dataType: 'json',
-		   data: s_urls+"js=1",
-		   success: cp_overide_add_click,
-		});
+		if(s_urls.length) cp_override_url_check(s_urls);
 
-		var confirm = new Array();
-		$("div#cp-content a.cp-confirm").each(function(){
-			confirm[$(this).attr('href')] = true;
-		});
-		cp_overide_add_click(confirm);
-	});
+//		var confirm = new Array();
+//		$(s_container+" a.cp-confirm:not(.cp_overide_checked)").each(function(){
+//			$(this).addClass('cp_overide_checked');
+//			confirm[$(this).attr('href')] = true;
+//		});
+//		cp_overide_add_click(confirm);
 }
 
 var cp_overide_add_click = function(return_value){
+	cp_override_processing_ajax = false;
+	
+	var s_container = $("div#cp-content").length ? "div#cp-content":"div.content";
 	for ( href in return_value.overideable_settings) {
 		if(return_value.overideable_settings[href]){
 			cp_add_dialog();
 
-			$("div#cp-content a[href^=\""+href.replace(/([#;&,\.\+\*\~':"\!\^$\[\]\(\)=>\|])/g, "\\$1")+"\"]").click(function(event){
+			$(s_container+" a[href^=\""+href.replace(/([#;&,\.\+\*\~':"\!\^$\[\]\(\)=>\|])/g, "\\$1")+"\"]").click(function(event){
 			  if(!$('#cp_confirm_dialog').dialog('isOpen')){
 				  var link = $(this);
 				  $('#cp_confirm_dialog span').html(link.html()+". Are you sure?");
@@ -52,7 +49,6 @@ var cp_overide_add_click = function(return_value){
 };
 
 var cp_add_dialog = function(){
-  //$('a.ui-dialog-titlebar-close span').addClass('close-this');
 	if(!$('#cp_confirm_dialog').length){
 		$('<div id="cp_confirm_dialog" title="Please Confirm"><p><span class="ui-icon ui-icon-alert">Are you sure?</span></p></div>').appendTo("body");
 		$('#cp_confirm_dialog').dialog({
@@ -79,3 +75,22 @@ var cp_add_dialog = function(){
  		  });
 	}
 };
+
+var cp_override_processing_ajax = false;
+//Make the AJAX call to check the urls
+var cp_override_url_check = function(s_urls){
+	if(cp_override_processing_ajax) return;
+	cp_override_processing_ajax = true;
+	$.ajax({
+	   type: "POST",
+	   url: location.pathname.substring(0,location.pathname.indexOf('/',1))+"/cp/allow_callback_override",
+	   dataType: 'json',
+	   data: s_urls+"js=1",
+	   success: cp_overide_add_click,
+    });
+};
+
+function cp_override_getEventTarget(e) { 
+  e = e || window.event; 
+  return e.target || e.srcElement; 
+} 
