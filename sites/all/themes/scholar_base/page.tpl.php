a3ce652e420b426c0941efe2aebec8130512c880
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-10-15 15:29:40 +0000

    footer login theming

--- sites/all/themes/scholar_base/page.tpl.php
+++ sites/all/themes/scholar_base/page.tpl.php
@@ -105,15 +105,11 @@
       <?php if ($footer) : ?>
         <?php print $footer; ?>
       <?php endif; ?>
-      <?php 
-
+      <?php
         global $user, $base_url;
         purl_disable(TRUE);
         $home_link =  l('The Scholars\' Web Sites Project, IQSS, Harvard University',$base_url);
-        $login_link = (!$user -> uid) ? l("Login", "user", array('absolute' => TRUE)) : "";
-
-        //$home_link 
-      
+        $login_link = (!$user -> uid) ? l("Login", "user", array('attributes' => array('class' => 'footer-login'),'absolute' => TRUE)) : "";
       ?>
       <p class="copy"><?php print $home_link; ?>. Copyright &copy; <?php echo date('Y');?> President &amp; Fellows of Harvard University. <?php print $login_link;?></p>
       </div> <!-- /#footer -->
