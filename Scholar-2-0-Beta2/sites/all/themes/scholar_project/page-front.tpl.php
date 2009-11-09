52d0b59f36893adbd57945530a62a8bf6061dc88
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2009-11-09 20:55:14 +0000

    initial release

---
+++ Scholar-2-0-Beta2/sites/all/themes/scholar_project/page-front.tpl.php
@@ -0,0 +1,62 @@
+<?php
+// $Id: page.tpl.php,v 1.10.2.4 2009/02/13 17:30:22 swortis Exp $
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+</head>
+<body class="<?php print $body_classes; ?>">
+  <div id="wrapper">
+    <div id="container">
+
+      <div id="header">
+        <div id="header-left" class="column">
+          <h1><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a></h1>
+        </div><!-- /header-left -->
+        <div id="header-right" class="column">
+          <?php print $header_right; ?>
+            <ul id="top-front">
+              <!--<li>What's New</li>
+              <li>Features</li>-->
+              <li><?php print l('About', 'help/vsitehelp/Scholar-Web-Sites-Project-Documentation'); ?> </li>
+              <li><?php print l('Log in', 'user');?></li>
+              <li id="get-form"><a href="site/register">Get your site now!</a></li>
+             </ul>
+         </div><!-- /header-right -->
+      </div> <!-- /header -->
+
+      <div id="content-wrapper">
+
+        <div id="content-main" class="column">
+            <?php if ($messages) print $messages; ?>
+            <?php if ($content) print $content; ?>
+
+              <?php print $content_top;?>
+
+
+
+          </div><!-- /content main -->
+      </div> <!-- / content wrapper -->
+
+      <div id="footer">
+      <?php if ($footer_message){ ?>
+        <div id="footer-message"><?php print $footer_message; ?></div>
+      <?php }; ?>
+      <?php if ($footer){ ?>
+        <?php print $footer; ?>
+      <?php }; ?>
+      </div> <!-- /#footer -->
+
+    </div> <!-- /container -->
+  </div> <!-- /wrapper -->
+  <?php if ($closure_region){ ?>
+    <div id="closure-blocks"><?php print $closure_region; ?></div>
+  <?php }; ?>
+  <?php print $closure; ?>
+</body>
+</html>
