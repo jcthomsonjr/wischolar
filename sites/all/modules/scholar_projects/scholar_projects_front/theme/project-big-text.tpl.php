246ba3770040316b9b1cc8ae60fa0fe40032d41a
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-01-28 15:40:26 +0000

    projects front

---
+++ sites/all/modules/scholar_projects/scholar_projects_front/theme/project-big-text.tpl.php
@@ -0,0 +1,5 @@
+  <div id="tutorial-link">See a video tutorial</div>
+  <div class="content">
+    <h2>Harvard Scholars: Create a dynamic feature-full web site in seconds. For free!</h2>
+    <h3>(No, there’s no catch.)</h3>
+ </div>
