6c4d6f26e7162ed1ba3c50ac0b031e1459d95cc4
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-01-29 21:55:52 +0000

    theming of front

--- sites/all/themes/scholar_projects_front/page-front.tpl.php
+++ sites/all/themes/scholar_projects_front/page-front.tpl.php
@@ -25,6 +25,7 @@
               <li>Features</li>
               <li><?php print l('About', 'help/vsitehelp/Scholar-Web-Sites-Project-Documentation'); ?> </li>-->
               <li><?php print  ($user->uid > 0) ? l('Log out', 'logout') :   l('Log in', 'user'); ?></li>
+              <li id="coming-soon">Coming Soon!</li>
               <!--<li id="get-form"><a href="site/register">Get your site now!</a></li>-->
              </ul>
          </div><!-- /header-right -->
