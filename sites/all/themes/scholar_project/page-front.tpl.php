e4cdf6af9ff71c92e3446fd5570a8eccd25a0a41
jjweiner <jjweiner@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-03-04 20:04:06 +0000

    added custom variable comments

--- sites/all/themes/scholar_project/page-front.tpl.php
+++ sites/all/themes/scholar_project/page-front.tpl.php
@@ -1,5 +1,14 @@
 <?php
 // $Id: page.tpl.php,v 1.10.2.4 2009/02/13 17:30:22 swortis Exp $
+
+/**
+ * @file page-front.tpl.php
+ * Theme implementation for displaying Scholars' Web Sites Project front page
+ * 
+ * Custom variables:
+ * - $front_button_id: ID name for <li> that displays the front link/button. Returns either 'get-form' or 'get-site'.
+ * - $front_button_link : Returns the <a href link for the front button either 'site/register', 'user' or $vsite ->purl
+ */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
