80dc1ad389f85602bf25acc7027f2569cfd7ed12
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-03-15 19:36:52 +0000

    small css bugfix after release from shaun

---
+++ SCHOLAR-2-0-BETA4-1/sites/all/modules/vsite/vsite_design/theme/vsite_design_theme_picker.js
@@ -0,0 +1,23 @@
+if (Drupal.jsEnabled) {
+  $(document).ready(function() {
+
+    var n_container = $('#cp-settings-form .form-radios .form-item').length;
+
+    $(".item-theme-picker:first").addClass('active');
+    $('div#prev,div#next,.theme_subnav li').cpSlidingContainer({
+        containerW: 600,
+        mainListSelector:'ul.theme-picker',
+        listItemClass: 'item-theme-picker',
+        navClass:'theme_subnav'
+        });
+
+    $('li.item-theme-picker').click(function(){
+    	// remove the active class from every li first
+    	$(".item-theme-picker").removeClass('checked');
+    	// add the class to this one
+    	$(this).addClass('checked');
+    	$("#edit-settings-theme-" + $(this).attr('id')).attr("checked", "checked").change();
+    });
+
+  });
+}
