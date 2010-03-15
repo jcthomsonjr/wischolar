7549b1884c3078b35333dd5dc5c5207d4ee53700
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-03-15 18:36:48 +0000

    create the production branch

---
+++ SCHOLAR-2-0-BETA4/sites/all/modules/iqss/iqss_project/theme/project-maintain.tpl.php
@@ -0,0 +1,5 @@
+<div id="feature-biblio" class="featureblock">
+  <h3>Maintain an accurate and  citable bibliography of all your writings.</h3>
+  <p>Automatically have your publications submitted to induces such as RePEc, Computer Science Repository, and Google Scholar.
+  </p>
+</div>
