e936be015d00414554023abca3e184ee67938521
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-03-18 19:35:04 +0000

    Multiple bug fixes including feed/views updates, cp js fixes, profile update, biblio teaser changes

---
+++ SCHOLAR-2-0-BETA4-3/sites/all/modules/iqss/iqss_project/theme/project-big-text.tpl.php
@@ -0,0 +1,8 @@
+  <div id="tutorial-link">Now at version 2.0!</div>
+  <div class="content">
+    <div id="video-link">
+    <object width="638" height="359"><param name="allowfullscreen" value="true" /><param name="allowscriptaccess" value="always" /><param name="movie" value="http://vimeo.com/moogaloop.swf?clip_id=9887585&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" /><embed src="http://vimeo.com/moogaloop.swf?clip_id=9887585&amp;server=vimeo.com&amp;show_title=0&amp;show_byline=0&amp;show_portrait=0&amp;color=00ADEF&amp;fullscreen=1" type="application/x-shockwave-flash" allowfullscreen="true" allowscriptaccess="always" width="638" height="359"></embed></object>
+    </div>
+
+    <h2>Harvard Scholars: Create a dynamic feature-full web site in seconds. For free! <span>(No, there’s no catch.)</span></h2>
+ </div>
