9f142b69772581430b155191ca46bfaec4bc0c56
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-03-18 16:14:13 +0000

    fix the js error that is caused if the ajax operation has not finished

--- sites/all/modules/vsite/vsite_design/theme/vsite_design_theme_picker.js
+++ sites/all/modules/vsite/vsite_design/theme/vsite_design_theme_picker.js
@@ -18,6 +18,24 @@ if (Drupal.jsEnabled) {
     	$(this).addClass('checked');
     	$("#edit-settings-theme-" + $(this).attr('id')).attr("checked", "checked").change();
     });
+    
+    $('#cp-settings-form').submit(function(event) {
+    	//Add the ajax spinner
+    	if(!$('#design-submit-waiting').length) $('#edit-submit').after('<div id="design-submit-waiting" class="ctools-ajaxing" count="0"> &nbsp; </div>');
+    	
+    	//Loop untill the ajax is done or we have waited 5 cycles
+    	if($('.ctools-ajaxing:not(#design-submit-waiting)').length > 0 && $('#design-submit-waiting').attr('count') < 6){
+    	  event.preventDefault();
+    	  $('#edit-submit').animate({ dummy: 1 }, 1000) //Replace this line with delay when we upgrade to jquery 1.4
+    	  .queue('fx',function(){
+    		$('#design-submit-waiting').attr('count',parseInt($('#design-submit-waiting').attr('count'))+1);
+    	    $('#cp-settings-form').submit();
+    	    $(this).dequeue();
+    	  });
+    	}
+    	return true;
+    });
+    
 
   });
 }
