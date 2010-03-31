03ab77361ca0ed279b7683d29c0c9037e301ef47
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-03-31 20:40:30 +0000

    delete all releases with the software

--- sites/all/modules/iqss/iqss_rbuild/iqss_rbuild.js
+++ sites/all/modules/iqss/iqss_rbuild/iqss_rbuild.js
@@ -9,7 +9,7 @@ Drupal.behaviors.iqss_rbuild = function() {
         case 'rbuild':
           $('#edit-field-scholar-software-repo-0-value').attr('disabled', 'disabled');
           $('#edit-field-scholar-software-repo-0-value').val(Drupal.settings.iqss_rbuild.repo_url); 
-          $('#edit-title').attr('disabled', 'disabled');
+          $('#edit-title-wrapper').hide();
           $('#edit-title').val(Drupal.settings.iqss_rbuild.title_default); 
           $('#edit-field-scholar-software-name-0-value-wrapper').show();
           $('#edit-body-wrapper').hide();
@@ -17,7 +17,7 @@ Drupal.behaviors.iqss_rbuild = function() {
         default:
         	$('#edit-field-scholar-software-repo-0-value').removeAttr('disabled');
             if(Drupal.settings.iqss_rbuild.repo_url && Drupal.settings.iqss_rbuild.repo_url == $('#edit-field-scholar-software-repo-0-value').val()) $('#edit-field-scholar-software-repo-0-value').val("");
-            $('#edit-title').removeAttr('disabled');
+            $('#edit-title-wrapper').show();
             if(Drupal.settings.iqss_rbuild.title_default && Drupal.settings.iqss_rbuild.title_default == $('#edit-title').val()) $('#edit-title').val("");
             $('#edit-field-scholar-software-name-0-value-wrapper').hide();
             $('#edit-body-wrapper').show();
