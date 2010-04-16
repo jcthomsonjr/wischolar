1fb2c4be8fc0a93d88d9450146d6a5bad4cc889b
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-04-16 13:48:08 +0000

    block mapping changes

---
+++ sites/all/modules/vsite/vsite_layout/theme/vsite_layout_gather_theme_data.js
@@ -0,0 +1,27 @@
+Drupal.behaviors.scholarlayoutGatherThemeData = function() {
+    var layoutRegions = { content_main:"#content-main", header_left:"#header-left", header_main:"#header-main", header_right:"#header-right", navbar:"#navbar", left:"#sidebar-left", right:"#sidebar-right"};
+
+    
+    scholar_layout_send_region_data(layoutRegions);
+
+};
+
+function scholar_layout_send_region_data(layoutRegions){
+	
+	var containerOffset = $('#container').offset();
+    $.each(layoutRegions, function(key, value){
+  	  var offset = $(value).offset();
+  	  
+  	  var data = {theme:Drupal.settings.scholarlayoutGatherThemeData.theme_name ,name:key, height: Math.min($(value).height(),350), width: $(value).width(), left:(offset.left - containerOffset.left), top:(offset.top - containerOffset.top)  };
+  	  
+  	  delete layoutRegions[key];
+  	  $.post(Drupal.settings.scholarlayoutGatherThemeData.theme_data_path, data,
+  	    function(data){
+  	      $('#content').append("<h4>Sent: "+data+"</h4>");
+  	    scholar_layout_send_region_data(layoutRegions);
+  	    }
+  	  );
+  	  return false;
+  	});
+	
+}
