d085cb301f1ea5ade62ad101c2c0782eb8a7b9c1
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-04-19 15:50:57 +0000

    add docx type

---
+++ SCHOLAR-2-0-BETA4-11/sites/all/modules/scholar_projects/scholar_projects_front/theme/project-wordout.tpl.php
@@ -0,0 +1,5 @@
+<div id="feature-wordout" class="featureblock">
+  <h3>Get the word out.</h3>
+  <p>Create project announcements, resources, links, RSS feeds.
+  </p>
+</div>
---
+++ SCHOLAR-2-0-BETA4-11/sites/all/themes/scholar_base/node-vsite_users_profile.tpl.php
@@ -0,0 +1,41 @@
+<?php
+// $Id: node.tpl.php,v 1.4 2008/09/15 08:11:49 johnalbin Exp $
+
+/**
+ * @file node.tpl.php
+ */
+?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
+  <div class="node-inner">
+    <?php print $picture; ?>
+    <?php if (!$page): ?>
+      <h3 class="title">
+        <a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a>
+      </h3>
+    <?php endif; ?>
+    <?php if ($unpublished): ?>
+      <div class="unpublished"><?php print t('Unpublished'); ?></div>
+    <?php endif; ?>
+    <?php if ($submitted or $terms): ?>
+      <div class="meta">
+        <?php if ($submitted): ?>
+          <div class="submitted">
+            <?php print $submitted; ?>
+          </div>
+        <?php endif; ?>
+        <?php if ($terms): ?>
+          <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
+    <div class="content">
+      <?php// print $content; ?>
+      <?php if ($field_user_profile_photo_rendered) {
+        print $field_user_profile_photo_rendered;
+        } ?>
+      <h3><?php print $vsite_users_first_name[0]['value'] . ' ' . $vsite_users_last_name[0]['value'];?></h3>
+      <?php print $vsite_users_title_rendered;?>
+    </div>
+    <?php print $links; ?>
+  </div> <!-- /node-inner -->
+</div> <!-- /node -->
