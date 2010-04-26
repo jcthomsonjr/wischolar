05470aa3b8a12f71785228c58ae4e723ef38c319
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-04-26 21:01:20 +0000

    missing files

--- sites/all/modules/iqss/iqss_project/theme/project-top-links.tpl.php
+++ sites/all/modules/iqss/iqss_project/theme/project-top-links.tpl.php
@@ -1,7 +1,7 @@
 <ul id="top-front">
   <li><?php print l('About', 'help/vsitehelp/Scholar-Web-Sites-Project-Documentation'); ?> </li>
   <li><?php print  ($user->uid > 0) ? l('Log out', 'logout') :   l('Log in', 'user'); ?></li>
-  <li><?php print l('Need a Personal Academic Site?', 'http://scholar.harvard.edu');?></li>
+  <li><?php print l('Need a Site for a Project?', 'http://projects.iq.harvard.edu');?></li>
   <li id="get-form"> <?php  if ($front_button_link ) { print $front_button_link; }
   else { print '<span id="coming-soon"> Coming Soon </span>'; } ?> </li>
 </ul>
