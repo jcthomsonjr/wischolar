5c8d6a2de76b8489a2de47902ebe8d284e73d380
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-05-07 15:41:35 +0000

    changes to biblio and gary theme

--- sites/all/themes/scholar_base/node-biblio.tpl.php
+++ sites/all/themes/scholar_base/node-biblio.tpl.php
@@ -8,11 +8,6 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
   <div class="node-inner">
     <?php print $picture; ?>
-    <?php if (!$page): ?>
-      <h3 class="title">
-        <a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a>
-      </h3>
-    <?php endif; ?>
     <?php if ($unpublished): ?>
       <div class="unpublished"><?php print t('Unpublished'); ?></div>
     <?php endif; ?>
