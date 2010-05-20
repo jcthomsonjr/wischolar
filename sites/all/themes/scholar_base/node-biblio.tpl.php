0784d9020bbedee6e8307db9d3b47a065de3a062
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-05-20 14:34:48 +0000

    adding book cover to biblio, twitter etc.

--- sites/all/themes/scholar_base/node-biblio.tpl.php
+++ sites/all/themes/scholar_base/node-biblio.tpl.php
@@ -6,26 +6,33 @@
  */
 ?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
-  <div class="node-inner">
-    <?php print $picture; ?>
-    <?php if ($unpublished): ?>
-      <div class="unpublished"><?php print t('Unpublished'); ?></div>
-    <?php endif; ?>
-    <?php if ($submitted or $terms): ?>
-      <div class="meta">
-        <?php if ($submitted): ?>
-          <div class="submitted">
-            <?php print $submitted; ?>
-          </div>
-        <?php endif; ?>
-        <?php if ($terms): ?>
-          <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
-        <?php endif; ?>
-      </div>
-    <?php endif; ?>
-    <div class="content">
-      <?php print $content; ?>
+  <?php if ($unpublished): ?>
+    <div class="unpublished"><?php print t('Unpublished'); ?></div>
+  <?php endif; ?>
+  <?php if ($submitted or $terms): ?>
+    <div class="meta">
+      <?php if ($submitted): ?>
+        <div class="submitted">
+          <?php print $submitted; ?>
+        </div>
+      <?php endif; ?>
+      <?php if ($terms): ?>
+        <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+      <?php endif; ?>
     </div>
-    <?php print $links; ?>
-  </div> <!-- /node-inner -->
+  <?php endif; ?>
+  <div class="content">
+    <?php if (!$page && $field_biblio_image): ?>
+    <div class="bookcover">
+      <?php print $field_biblio_image_rendered;?>
+    </div>
+  <?php endif;?>
+    <?php print $content; ?>
+    <?php if ($page && $field_biblio_image): ?>
+    <div class="bookcover">
+      <?php print $field_biblio_image_rendered;?>
+    </div>
+  <?php endif;?>
+  </div>
+  <?php print $links; ?>
 </div> <!-- /node -->
