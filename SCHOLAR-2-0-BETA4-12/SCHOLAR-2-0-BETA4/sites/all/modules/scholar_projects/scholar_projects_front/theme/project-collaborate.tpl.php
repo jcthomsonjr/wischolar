56e28d6125c3803b838ffb69d649564e58e2280d
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-05-26 18:11:39 +0000

    add 'thedata' theme and fix private rbates

---
+++ SCHOLAR-2-0-BETA4-12/SCHOLAR-2-0-BETA4/sites/all/modules/scholar_projects/scholar_projects_front/theme/project-collaborate.tpl.php
@@ -0,0 +1,4 @@
+<div id="feature-collaborate" class="featureblock">
+  <h3>Collaborate with your colleagues.</h3>
+  <p>Easily build site content together. Manage Twitter and news feeds.</p>
+</div>
