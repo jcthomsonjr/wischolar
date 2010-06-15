a5441df04f8a925307fb641e90154cf2f15f961f
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-06-15 15:18:54 +0000

    add fserver xml

--- sites/all/modules/iqss/iqss_rbuild/iqss_rbuild.js
+++ sites/all/modules/iqss/iqss_rbuild/iqss_rbuild.js
@@ -10,8 +10,8 @@ Drupal.behaviors.iqss_rbuild = function() {
           $('#edit-field-scholar-software-repo-0-value').attr('disabled', 'disabled');
           $('#edit-field-scholar-software-repo-0-value').val(Drupal.settings.iqss_rbuild.repo_url); 
           $('#edit-title-wrapper').hide();
-          $('#edit-title').val(Drupal.settings.iqss_rbuild.title_default); 
-          $('#edit-field-scholar-software-name-0-value-wrapper').show();
+          //$('#edit-title').val(Drupal.settings.iqss_rbuild.title_default); 
+          //$('#edit-field-scholar-software-name-0-value-wrapper').show();
           $('#edit-body-wrapper').hide();
           break;
         default:
@@ -19,7 +19,7 @@ Drupal.behaviors.iqss_rbuild = function() {
             if(Drupal.settings.iqss_rbuild.repo_url && Drupal.settings.iqss_rbuild.repo_url == $('#edit-field-scholar-software-repo-0-value').val()) $('#edit-field-scholar-software-repo-0-value').val("");
             $('#edit-title-wrapper').show();
             if(Drupal.settings.iqss_rbuild.title_default && Drupal.settings.iqss_rbuild.title_default == $('#edit-title').val()) $('#edit-title').val("");
-            $('#edit-field-scholar-software-name-0-value-wrapper').hide();
+            //$('#edit-field-scholar-software-name-0-value-wrapper').hide();
             $('#edit-body-wrapper').show();
           break;
       }
