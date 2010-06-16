60cafe8805e88daca84df5fb09bd36727b1c1eba
jjweiner <jjweiner@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-06-16 17:25:31 +0000

    updated the About link to new name for top level documentation

--- sites/all/modules/scholar_projects/scholar_projects_front/theme/project-top-links.tpl.php
+++ sites/all/modules/scholar_projects/scholar_projects_front/theme/project-top-links.tpl.php
@@ -1,5 +1,5 @@
 <ul id="top-front">
-  <li><?php print l('About', 'help/vsitehelp/Scholar-Web-Sites-Project-Documentation'); ?> </li>
+  <li><?php print l('About', 'help/vsitehelp/OpenScholar-Documentation'); ?> </li>
   <li><?php print  ($user->uid > 0) ? l('Log out', 'logout') :   l('Log in', 'user'); ?></li>
   <li><?php print l('Need a Personal Academic Site?', 'http://scholar.harvard.edu');?></li>
   <li id="get-form"> <?php  if ($front_button_link ) { print $front_button_link; }
