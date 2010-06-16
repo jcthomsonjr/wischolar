861337a0e75bfd91a9e30a169bdf380ac7b68783
jjweiner <jjweiner@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-06-16 18:07:29 +0000

    updated project name to OpenScholar

--- sites/all/modules/iqss/iqss_project/theme/project-site-examples.tpl.php
+++ sites/all/modules/iqss/iqss_project/theme/project-site-examples.tpl.php
@@ -5,8 +5,8 @@
     <div class="photo">
       <?php print $site['photo'];?>
      </div>
-   <h4><?php print $site['headline'];?></h4>
-   <h5><?php print $site['sub_headline'];?></h5>
+   <h4><?php //print $site['headline'];?></h4>
+   <h5><?php // print $site['sub_headline'];?></h5>
    </div>
 <?php endforeach;?>
 
