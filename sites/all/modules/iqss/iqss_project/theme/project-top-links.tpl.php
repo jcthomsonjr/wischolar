80d56255110b0044d13e91a683daeab89f6bc9fc
jjweiner <jjweiner@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-06-16 17:15:21 +0000

    updated the About link to new name for top level documentation

--- sites/all/modules/iqss/iqss_project/theme/project-top-links.tpl.php
+++ sites/all/modules/iqss/iqss_project/theme/project-top-links.tpl.php
@@ -1,5 +1,5 @@
 <ul id="top-front">
-  <li><?php print l('About', 'help/vsitehelp/Scholar-Web-Sites-Project-Documentation'); ?> </li>
+  <li><?php print l('About', 'help/vsitehelp/OpenScholar-Documentation'); ?> </li>
   <li><?php print  ($user->uid > 0) ? l('Log out', 'logout') :   l('Log in', 'user'); ?></li>
   <li><?php print l('Need a Site for a Project?', 'http://projects.iq.harvard.edu');?></li>
   <li id="get-form"> <?php  if ($front_button_link ) { print $front_button_link; }
