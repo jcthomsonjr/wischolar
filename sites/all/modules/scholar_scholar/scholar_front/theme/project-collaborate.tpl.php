4c5c60ebb143029c8d69bfb961e8715ef5611c43
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-07-07 14:08:55 +0000

    conflict with scholar_front feature

--- sites/all/modules/scholar_scholar/scholar_front/theme/project-collaborate.tpl.php
+++
@@ -1,4 +0,0 @@
-<div id="feature-collaborate" class="featureblock">
-  <h3>Collaborate with your colleagues.</h3>
-  <p>Allow commentary on your work by whomever you select. Manage your Twitter feeds; keep track of colleagues’ publications and blogs.</p>
-</div>
