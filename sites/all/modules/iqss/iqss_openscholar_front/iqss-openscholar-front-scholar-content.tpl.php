c876f5c141680c3697402f6fb621cde440546a7a
alimadhi <alimadhi@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-07-08 16:37:15 +0000

    fix typo

--- sites/all/modules/iqss/iqss_openscholar_front/iqss-openscholar-front-scholar-content.tpl.php
+++ sites/all/modules/iqss/iqss_openscholar_front/iqss-openscholar-front-scholar-content.tpl.php
@@ -16,5 +16,5 @@
 
 <div id="feature-collaborate" class="featureblock">
   <h3>Collaborate with your colleagues.</h3>
-  <p>Allow commentary on your work by whomever you select. Manage your Twitter feeds; keep track of colleagues’ publications and blogs.</p>
+  <p>Allow commentary on your work by whomever you select. Manage your Twitter feeds; keep track of colleagues, publications and blogs.</p>
 </div>
