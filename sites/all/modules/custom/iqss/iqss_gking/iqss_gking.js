3817b3b23a27f6aa30c222e97505d2b0e33c0e27
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-07-14 21:32:10 +0000

    gking with effects

--- sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
+++ sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
@@ -1,13 +1,36 @@
-  $(document).ready(function(){
-    $('.col dt span').hover(function() {
-      $(this).after('<div class="bg"></div>');
-      $(this).parent('dt').addClass('active');
-      $(this).children('.more').fadeIn("fast");
-      $(this).parent('dt').next('dd').fadeIn(180);
-      }, function() {
-        $(this).next('.bg').remove();
-        $(this).parent('dt').removeClass('active');
-        $(this).children('.more').fadeOut("fast");
-        $(this).parent('dt').next('dd').fadeOut(180);
-        });
-      });
+  //Javascript to manipulate the Areas of Interest block on Garys Home Page
+$(document).ready(function(){
+  $('#block-iqss_gking-areas_of_research .col dt span').hover(function() {
+	window.clearTimeout(timer_block_iqss_gking_areas_of_research);
+	$('#block-iqss_gking-areas_of_research .bg').remove();
+	$('#block-iqss_gking-areas_of_research dt.active').removeClass('active');
+	$('#block-iqss_gking-areas_of_research dd').fadeOut(180);
+	$('#block-iqss_gking-areas_of_research .more').hide();
+	  
+    $(this).after('<div class="bg"></div>');
+    $(this).parent('dt').addClass('active');
+    $(this).children('.more').fadeIn("fast");
+    $(this).parent('dt').next('dd').fadeIn(820);
+  }, function() {
+    
+    window.clearTimeout(timer_block_iqss_gking_areas_of_research);
+    timer_block_iqss_gking_areas_of_research = window.setTimeout(iqss_gking_areas_of_research_load_next, 5000);
+  });
+  iqss_gking_areas_of_research_load_next();
+});
+
+var timer_block_iqss_gking_areas_of_research = false;
+function iqss_gking_areas_of_research_load_next(){
+	var next_el = false;
+	var found_active = false;
+	$('#block-iqss_gking-areas_of_research .col dt span').each(function(index) {
+		if(!next_el) next_el = $(this);
+		if(found_active){
+			next_el = $(this);
+			found_active = false;
+		}
+		
+		if($(this).parent('dt').hasClass('active')) found_active = true;
+	});
+	next_el.hover();
+}
