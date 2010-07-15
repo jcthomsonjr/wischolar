92321c40cab64445a3da5e64741d0806bc4b7eb8
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-07-15 19:35:12 +0000

    fix hover bug

--- sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
+++ sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
@@ -1,7 +1,7 @@
   //Javascript to manipulate the Areas of Interest block on Garys Home Page
 $(document).ready(function(){
   $('#block-iqss_gking-areas_of_research .col dt span').hover(function() {
-	window.clearTimeout(timer_block_iqss_gking_areas_of_research);
+	//window.clearTimeout(timer_block_iqss_gking_areas_of_research);
 	$('#block-iqss_gking-areas_of_research .bg').remove();
 	$('#block-iqss_gking-areas_of_research dt.active').removeClass('active');
 	$('#block-iqss_gking-areas_of_research dd').fadeOut(180);
@@ -13,14 +13,17 @@ $(document).ready(function(){
     $(this).parent('dt').next('dd').fadeIn(820);
   }, function() {
     
-    window.clearTimeout(timer_block_iqss_gking_areas_of_research);
-    timer_block_iqss_gking_areas_of_research = window.setTimeout(iqss_gking_areas_of_research_load_next, 9000);
+    //window.clearTimeout(timer_block_iqss_gking_areas_of_research);
+    //timer_block_iqss_gking_areas_of_research = window.setTimeout(iqss_gking_areas_of_research_load_next, 9000);
   });
   iqss_gking_areas_of_research_load_next();
+  var timer_block_iqss_gking_areas_of_research = window.setInterval(iqss_gking_areas_of_research_load_next, 9000);
 });
 
-var timer_block_iqss_gking_areas_of_research = false;
+ false;
 function iqss_gking_areas_of_research_load_next(){
+	if($('#block-iqss_gking-areas_of_research .col dt span:hover').length) return;
+	
 	var next_el = false;
 	var found_active = false;
 	$('#block-iqss_gking-areas_of_research .col dt span').each(function(index) {
