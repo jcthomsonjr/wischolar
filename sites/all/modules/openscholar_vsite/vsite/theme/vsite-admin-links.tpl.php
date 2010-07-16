004e1163815358fcb22a69467afa8d16c224369b
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-07-16 18:00:26 +0000

    changes to gary theme plus admin menu etc

--- sites/all/modules/openscholar_vsite/vsite/theme/vsite-admin-links.tpl.php
+++ sites/all/modules/openscholar_vsite/vsite/theme/vsite-admin-links.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($links): ?>
-<div class="toggle"><span>Admin Options</span></div>
+<div class="toggle"><span>admin</span></div>
 <div class="vsite-admin-inline slider">
   <?php print $links ?>
 </div>
