a3567b99e365f95023242f5cd1fb16dbcad03760
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-07-29 20:17:15 +0000

    changes to gary site, admin menu, wysiwyg

--- sites/all/modules/openscholar_vsite/vsite/theme/vsite.js
+++ sites/all/modules/openscholar_vsite/vsite/theme/vsite.js
@@ -5,4 +5,10 @@ Drupal.behaviors.scholar = function (context){
 
        return false;
   });
+
+  $('.disto-toggle', context).click(function(){
+	   $(this).parents('.ctools-dropdown-container').css({width:'auto'}).animate({width:342},250);
+     $(this).next('.slider').slideToggle("fast");
+       return false;
+  });
 }
