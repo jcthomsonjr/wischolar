d3d5c97880165ec1ffb472cbf9b5c14e47259ce6
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-08-24 20:17:46 +0000

    tag iqss point release

---
+++ SCHOLAR-2-0-BETA8-1/sites/all/themes/custom/gking/gking.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('#navbar li a').hover(
+      function () {
+          $(this).stop().animate({backgroundColor:'#fa8b18'}, 300);
+          }, function () {
+          $(this).stop().animate({backgroundColor:'#db7002'}, 100);
+      });
+
+   $('#navbar h1 a').hover(
+      function () {
+          $(this).stop().animate({backgroundColor:'#000000'}, 300);
+          }, function () {
+          $(this).stop().animate({backgroundColor:'#636363'}, 100);
+      });
+
+  });
