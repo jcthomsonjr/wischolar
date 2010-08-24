988132b37ab30ff3aec4e82b5d4cd7417101fcb1
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-08-24 20:17:55 +0000

    tag public point release

---
+++ SCHOLAR-2-0-BETA8-1/sites/all/modules/openscholar_features/scholar_publications/node-biblio.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id: node.tpl.php,v 1.4 2008/09/15 08:11:49 johnalbin Exp $
+
+/**
+ * @file node.tpl.php
+ */
+?>
+
+<?php print $content; ?>
+<span class="links-outer"><?php print $links; ?></span>
