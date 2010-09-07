8e6392007161fe70a344cffc11dbd5ef358dadd3
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-09-07 20:18:24 +0000

    add logo and rbuild updates

--- sites/all/modules/custom/iqss/iqss_rbuild/iqss_rbuild.js
+++ sites/all/modules/custom/iqss/iqss_rbuild/iqss_rbuild.js
@@ -9,12 +9,22 @@ Drupal.behaviors.iqss_rbuild = function() {
         case 'rbuild':
           $('#edit-field-scholar-software-repo-0-value').attr('disabled', 'disabled');
           $('#edit-field-scholar-software-repo-0-value').val(Drupal.settings.iqss_rbuild.repo_url); 
+          
+          if(!$('#rbuild-autopopulate-wrapper').length) $('#edit-field-scholar-software-method-value-wrapper').after('<div class="form-item" id="rbuild-autopopulate-wrapper"><label for="rbuild-autopopulate">Automatically Populate Fields:</label><input class="form-checkbox" id="rbuild-autopopulate" type="checkbox" checked="checked" /><div class="description">Auto populate this projects title and body fields from the rbuild repository.</div></div>');
+          $('#rbuild-autopopulate').change(function() { 
+        	  if ($('#rbuild-autopopulate').attr('checked')) {
+        		$('#edit-title-wrapper').hide();
+                $('#edit-body-wrapper').hide();
+              }else{
+                $('#edit-title-wrapper').show();
+                $('#edit-body-wrapper').show();
+              }
+          });
           $('#edit-title-wrapper').hide();
-          //$('#edit-title').val(Drupal.settings.iqss_rbuild.title_default); 
-          //$('#edit-field-scholar-software-name-0-value-wrapper').show();
           $('#edit-body-wrapper').hide();
           break;
         default:
+        	$('#rbuild-autopopulate-wrapper').remove();
         	$('#edit-field-scholar-software-repo-0-value').removeAttr('disabled');
             if(Drupal.settings.iqss_rbuild.repo_url && Drupal.settings.iqss_rbuild.repo_url == $('#edit-field-scholar-software-repo-0-value').val()) $('#edit-field-scholar-software-repo-0-value').val("");
             $('#edit-title-wrapper').show();
