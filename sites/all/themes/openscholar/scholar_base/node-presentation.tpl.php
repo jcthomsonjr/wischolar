5c835bebd5557a392cebeb5054a3e957e3dde620
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-09-09 18:21:41 +0000

    mostly gary stuff

--- sites/all/themes/openscholar/scholar_base/node-presentation.tpl.php
+++ sites/all/themes/openscholar/scholar_base/node-presentation.tpl.php
@@ -1,35 +1,45 @@
-<?php
-// $Id: node.tpl.php,v 1.4 2008/09/15 08:11:49 johnalbin Exp $
-
-/**
- * @file node.tpl.php
- */
-?>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
   <div class="node-inner">
-    <?php print $picture; ?>
+
     <?php if (!$page): ?>
+      <?php if ($node->field_presentation_date[0]['value'] || $terms) : ?>
+      <div class="presentation-meta">
+        <?php if ($node->field_presentation_date[0]['value']) : ?>
+        <div class="dates">
+        <?php print $node->field_presentation_date[0]['view'];?>
+        </div>
+        <?php endif; ?>
+        <?php if ($terms): ?>
+        <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+        <?php endif; ?>
+       <?php endif; ?>
+       </div>
+
+      <div class="presentation-content">
       <h3 class="title">
-        <a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a>
-      </h3>
-    <?php endif; ?>
+        <a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a></h3>
+      <?php endif; ?>
+
+    <?php if ($page): ?>
+
     <?php if ($unpublished): ?>
       <div class="unpublished"><?php print t('Unpublished'); ?></div>
     <?php endif; ?>
 
-     <?php if ($page): ?>
      <?php if ($terms): ?>
        <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
      <?php endif; ?>
-     <?php endif; ?>
     <div class="content">
-      <?php print $content; ?>
-    </div>
-    <?php if (!$page): ?>
-      <?php if ($terms): ?>
-      <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
-      <?php endif; ?>
     <?php endif; ?>
+
+
+      <?php print $content; ?>
+    <?php if ($page): ?>
+    </div>
+    <?php endif;?>
     <?php print $links; ?>
+    <?php if (!$page): ?>
+    </div>
+    <?php endif;?>
   </div> <!-- /node-inner -->
 </div> <!-- /node -->
