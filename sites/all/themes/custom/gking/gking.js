7f0dafd95d7ec7f32f7ae2d762da5e44c60fea86
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-09-10 18:34:55 +0000

    gary stuff

--- sites/all/themes/custom/gking/gking.js
+++ sites/all/themes/custom/gking/gking.js
@@ -1,3 +1,29 @@
+(function($) {
+
+   $.fn.dropshadow = function(settings) {
+     var config = $(settings).extend({x:0,y:.4,blur:4,color:'rgba(0,0,0,.3)'},settings);
+
+     if (settings) $.extend(config, settings);
+
+     this.each(function() {
+       $(this).hover(function(){
+						$(this).css('-webkit-box-shadow',config.x+'em '+config.y+'em '+config.blur+'em '+config.color);
+						$(this).css('-moz-box-shadow',config.x+'em '+config.y+'em '+config.blur+'em '+config.color);
+						$(this).css('box-shadow',config.x+'em '+config.y+'em '+config.blur+'em '+config.color);
+					},
+					function(){
+						$(this).css('-webkit-box-shadow', null);
+						$(this).css('-moz-box-shadow', null);
+						$(this).css('box-shadow', null);
+					});
+     });
+
+     return this;
+
+   };
+
+ })(jQuery);
+
 $(document).ready(function() {
   $('#navbar li a').hover(
       function () {
@@ -12,5 +38,14 @@ $(document).ready(function() {
           }, function () {
           $(this).stop().animate({backgroundColor:'#636363'}, 100);
       });
+
+    $('#block-vsite_menus-1 ul.menu li a').hover(
+      function () {
+          $(this).stop().animate({backgroundColor:'#F58714'}, 300);
+          }, function () {
+          $(this).stop().animate({backgroundColor:'#ffffff'}, 100);
+      });
+
+    $('#content, .column .block').dropshadow();
 
   });
