d3c42f6440448e472f0fb275e9bb7a3b9978ba67
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-09-13 20:38:38 +0000

    tag iqss point release

---
+++ SCHOLAR-2-0-BETA8-3.2/sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
@@ -0,0 +1,57 @@
+  //Javascript to manipulate the Areas of Interest block on Garys Home Page
+$(document).ready(function(){
+
+  $('#block-iqss_gking-areas_of_research .col').hover(function() {
+    $(this).siblings('.col').fadeTo('slow',.3);
+    }, function() {
+   $(this).siblings('.col').fadeTo(180,1);
+    });
+
+  $('#block-iqss_gking-areas_of_research .col dt span').hover(function() {
+    //Rem 1 for Sticky
+	window.clearTimeout(timer_block_iqss_gking_areas_of_research);
+  //$(this).parents('.col').siblings('.col').queue("fx",[]);
+	$('#block-iqss_gking-areas_of_research .bg').remove();
+	$('#block-iqss_gking-areas_of_research dt.active').removeClass('active');
+	$('#block-iqss_gking-areas_of_research dd').fadeOut(180);
+	$('#block-iqss_gking-areas_of_research .more').hide();
+    $(this).after('<div class="bg"></div>');
+    $(this).parent('dt').addClass('active');
+    $(this).children('.more').fadeIn("fast");
+    $(this).parent('dt').next('dd').fadeIn(820);
+
+  }, function() {
+    //Rem if for stickey
+	//if($('#block-iqss_gking-areas_of_research .col dt span').index($(this)) != 0){
+	  // Use This if you want hover
+	  //$(this).next('.bg').remove();
+	  //$(this).parent('dt').removeClass('active');
+	  //$(this).children('.more').fadeOut("fast");
+	  //$(this).parent('dt').next('dd').fadeOut(180);
+
+	  //Select First code
+	  //window.setTimeout(iqss_gking_areas_of_research_load_next, 290);
+	//}
+  });
+  iqss_gking_areas_of_research_load_next();
+  //var timer_block_iqss_gking_areas_of_research = window.setInterval(iqss_gking_areas_of_research_load_next, 9000);
+  var timer_block_iqss_gking_areas_of_research = null;
+});
+
+ false;
+function iqss_gking_areas_of_research_load_next(){
+	if($('#block-iqss_gking-areas_of_research .col dt span:hover').length) return;
+
+	var next_el = false;
+	var found_active = false;
+	$('#block-iqss_gking-areas_of_research .col dt span').each(function(index) {
+		if(!next_el) next_el = $(this);
+		if(found_active){
+			next_el = $(this);
+			found_active = false;
+		}
+
+		if($(this).parent('dt').hasClass('active')) found_active = true;
+	});
+	next_el.hover();
+}
