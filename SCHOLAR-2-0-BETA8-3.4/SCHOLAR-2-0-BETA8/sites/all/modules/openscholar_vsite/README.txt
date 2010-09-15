f31c7c89bacb3135ee80a101988b6649b345eeff
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-09-15 20:03:38 +0000

    tag iqss point release

---
+++ SCHOLAR-2-0-BETA8-3.4/SCHOLAR-2-0-BETA8/sites/all/modules/openscholar_vsite/README.txt
@@ -0,0 +1,3 @@
+$Id: README.txt,v 1.1.2.1 2010/07/28 02:51:24 ferdi Exp $
+
+
---
+++ SCHOLAR-2-0-BETA8-3.4/SCHOLAR-2-0-BETA8/sites/all/modules/openscholar_vsite/vsite/theme/vsite.js
@@ -0,0 +1,34 @@
+Drupal.behaviors.scholar = function (context){
+  $('.toggle').click(function(){
+	   $(this).toggleClass("expanded")
+
+	   if(!$.browser.msie) {
+         $(this).nextAll('.slider').slideToggle("fast");
+	   }else{
+		 //IE8 Does not work with the slider
+	     if ($(this).hasClass('expanded')) {
+		   $(this).nextAll('.slider').show();
+	     }
+	     else {
+		     $(this).nextAll('.slider').hide();
+	     }
+	   }
+	   return false;
+  });
+
+  $('div.node li.vsite-admin div.ctools-dropdown-container, div.biblio-entry li.vsite-admin div.ctools-dropdown-container').hover(function(){
+	var parent = $(this).parents('div.node');
+	if(!parent.length) parent = $(this).parents('div.biblio-entry');
+	parent.prepend('<div class="active-marker"></div>');
+  },function(){
+	var parent = $(this).parents('div.node');
+	if(!parent.length) parent = $(this).parents('div.biblio-entry');
+	parent.find('div.active-marker').remove();
+  });
+
+  $('.disto-toggle', context).click(function(){
+	   $(this).parents('.ctools-dropdown-container').css({width:'auto'}).animate({width:342},250);
+     $(this).next('.slider').slideToggle("fast");
+       return false;
+  });
+}
