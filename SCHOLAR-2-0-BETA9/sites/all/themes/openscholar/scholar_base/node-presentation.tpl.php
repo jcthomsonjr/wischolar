387ac5b1f50ef09073a23c5bbfc39f76c57fa3c0
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-11-03 16:26:36 +0000

    tag public release

---
+++ SCHOLAR-2-0-BETA9/sites/all/themes/openscholar/scholar_base/node-presentation.tpl.php
@@ -0,0 +1,45 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>">
+  <div class="node-inner">
+
+    <?php if (!$page): ?>
+      <?php if ($node->field_presentation_date[0]['value'] || $terms) : ?>
+      <div class="presentation-meta">
+        <?php if ($node->field_presentation_date[0]['value']) : ?>
+        <div class="dates">
+        <?php print $node->field_presentation_date[0]['view'];?>
+        </div>
+        <?php endif; ?>
+        <?php if ($terms): ?>
+        <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+        <?php endif; ?>
+       <?php endif; ?>
+       </div>
+
+      <div class="presentation-content">
+      <h3 class="title">
+        <a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a></h3>
+      <?php endif; ?>
+
+    <?php if ($page): ?>
+
+    <?php if ($unpublished): ?>
+      <div class="unpublished"><?php print t('Unpublished'); ?></div>
+    <?php endif; ?>
+
+     <?php if ($terms): ?>
+       <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+     <?php endif; ?>
+    <div class="content">
+    <?php endif; ?>
+
+
+      <?php print $content; ?>
+    <?php if ($page): ?>
+    </div>
+    <?php endif;?>
+    <?php print $links; ?>
+    <?php if (!$page): ?>
+    </div>
+    <?php endif;?>
+  </div> <!-- /node-inner -->
+</div> <!-- /node -->
