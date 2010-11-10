5c387f962524a5bb9c49fcdcd3a2cd8cfb13c084
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-11-10 18:01:11 +0000

    another change to this shit

--- sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
+++ sites/all/modules/custom/iqss/iqss_gking/iqss_gking.js
@@ -1,29 +1,28 @@
 //Javascript to manipulate the Areas of Interest block on Garys Home Page
-$(document).ready(function(){
+Drupal.behaviors.iqss_gking = function() {
 	
   iqss_gking_research_group_tabify();
 
   $('#block-iqss_gking-areas_of_research .col dt span').hover(function() {
     //Rem 1 for Sticky
 	window.clearTimeout(timer_block_iqss_gking_areas_of_research);
-  //$(this).parents('.col').siblings('.col').queue("fx",[]);
-	//$('#block-iqss_gking-areas_of_research .bg').remove();
-	$('#block-iqss_gking-areas_of_research dt.active').removeClass('active');
+  
+    $('#block-iqss_gking-areas_of_research dt.active').removeClass('active');
 	$('#block-iqss_gking-areas_of_research dd').fadeOut('fast');
 	$('#block-iqss_gking-areas_of_research .more').hide();
-    //$(this).after('<div class="bg"><\/div>');
+    
     $(this).parent('dt').addClass('active');
-    //$(this).children('.more').fadeIn("fast");
+
     $(this).parent('dt').next('dd').fadeIn('fast');
 
   });
   
   iqss_gking_areas_of_research_load_next();
-  //var timer_block_iqss_gking_areas_of_research = window.setInterval(iqss_gking_areas_of_research_load_next, 9000);
+
   var timer_block_iqss_gking_areas_of_research = null;
   
   
-});
+};
 
 function iqss_gking_areas_of_research_load_next(){
   if($('#block-iqss_gking-areas_of_research .col dt span:hover').length ||
@@ -47,13 +46,16 @@ function iqss_gking_research_group_tabify(){
 	if(!$('body.page-iqss-gking-research-group').length) return;
 	
 	$("<ul id='research-group-tablist' class=\"ui-tabs-nav\"></ul>").insertBefore("div#content-main div.taxonomy-term-child:first");
-	$('div#content-main div.taxonomy-term-child').each(function(index) {
-	  $(this).attr("id","research_group_tab_"+index);
-	  if(index > 0) $(this).hide();
-	  
-	  $('ul#research-group-tablist').append("<li class='research_group_tab'><a href='#research_group_tab_"+index+"'>"+$(this).find('h3.taxonomy-heading').html()+"</a></li>");
-	});
 	
-	$("#content").tabs({ fx: { opacity: 'toggle' } });
+	var terms = Drupal.settings.iqss_gking.research_group_terms;
+	for (var i=0;i<terms.length;i++)
+	{
+	  var url = (i > 0)? Drupal.settings.iqss_gking.research_group_ajax_url+"/"+terms[i].tid: "#iqss_gking_term_"+terms[i].tid;
+	  
+	  $('ul#research-group-tablist').append("<li class='research_group_tab'><a href='"+url+"'>"+terms[i].name+"</a></li>");
+	}
+
+	
+	$("#content").tabs({ fx: { opacity: 'toggle' }, load: function(event, ui) { Drupal.behaviors.CToolsDropdown(); } });
 
 }
