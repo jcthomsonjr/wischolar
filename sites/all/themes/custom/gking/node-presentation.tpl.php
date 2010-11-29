cac05ac0adc338ceee91cfe4990b92f3387b06f1
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-11-29 17:28:31 +0000

    presentation changes for gary

--- sites/all/themes/custom/gking/node-presentation.tpl.php
+++ sites/all/themes/custom/gking/node-presentation.tpl.php
@@ -2,17 +2,17 @@
   <div class="node-inner">
 
     <?php if (!$page): ?>
-      <span class="title"><a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a></span>
+      <span class="title"><a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a><?php if ($node->field_presentation_location[0]['value']) { print ', '; }?></span>
+      <?php if ($node->field_presentation_location[0]['value']):?>
       <span class="location"><?php print $node->field_presentation_location[0]['value']; if ($node->field_presentation_date[0]['value']) { print ', '; } ?></span>
       <?php endif; ?>
       <?php if ($node->field_presentation_date[0]['value']) {
         print $node->field_presentation_date[0]['view'];
-          if ($node->field_presentation_location[0]['value']) {
-          print ', ';
+          if ($node->field_presentation_file[0]['fid']) {
+          print ': ';
           }
         }
       ?>
-      <?php if ($node->field_presentation_location[0]['value']) : ?>
       <?php if ($node->field_presentation_file[0]['fid']) {
         foreach ($node->field_presentation_file as $file) {
           print $file['view'];
