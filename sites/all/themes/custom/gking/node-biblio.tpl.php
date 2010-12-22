467ea3b4b8282db1c0ff3698d95939bcd99eb122
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-12-22 16:44:34 +0000

    adding tpl files from production to trunk

---
+++ sites/all/themes/custom/gking/node-biblio.tpl.php
@@ -0,0 +1,27 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $node_classes; ?>">
+  <div class="node-inner">
+    <?php if ($unpublished): ?>
+      <div class="unpublished"><?php print t('Unpublished'); ?></div>
+    <?php endif; ?>
+    <?php if ($submitted): ?>
+      <div class="submitted">
+        <?php print $submitted; ?>
+      </div>
+    <?php endif; ?>
+
+    <?php
+    if (!$page){
+      //If teaser unify with views by using biblio entry theme
+      print theme( 'scholar_publications_biblio_entry' ,$node, variable_get('biblio_base', 'biblio'), biblio_get_style(), ( variable_get('biblio_inlinemode_in_links',0)? true : false), false);
+    } if ($page) { ?>
+
+      <div class="content">
+        <?php print $content; ?>
+      </div>
+      <?php if ($terms): ?>
+        <div class="terms terms-inline"><?php print t('See related: ') . $terms; ?></div>
+      <?php endif; ?>
+      <?php print $links; ?>
+    <?php } ?>
+  </div> <!-- /node-inner -->
+</div> <!-- /node -->
---
+++ sites/all/themes/custom/gking/node-presentation.tpl.php
@@ -0,0 +1,40 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $node_classes; ?>">
+  <div class="node-inner">
+
+    <?php if (!$page): ?>
+      <span class="title"><a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a><?php if ($node->field_presentation_location[0]['value']) { print ', '; }?></span>
+      <?php if ($node->field_presentation_location[0]['value']):?>
+      <span class="location"><?php print $node->field_presentation_location[0]['value']; if ($node->field_presentation_date[0]['value']) { print ', '; } ?></span>
+      <?php endif; ?>
+      <?php if ($node->field_presentation_date[0]['value']) {
+        print $node->field_presentation_date[0]['view'];
+          if ($node->field_presentation_file[0]['fid']) {
+          print ': ';
+          }
+        }
+      ?>
+      <?php if ($node->field_presentation_file[0]['fid']) {
+        foreach ($node->field_presentation_file as $file) {
+          print $file['view'];
+          }
+        }
+      ?>
+     <?php endif; ?>
+
+
+    <?php if ($page): ?>
+      <?php if ($unpublished): ?>
+      <div class="unpublished"><?php print t('Unpublished'); ?></div>
+      <?php endif; ?>
+
+      <?php if ($terms): ?>
+      <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+      <?php endif; ?>
+
+      <div class="content">
+        <?php print $content; ?>
+      </div>
+      <?php endif;?>
+      <?php print $links; ?>
+  </div> <!-- /node-inner -->
+</div> <!-- /node -->
