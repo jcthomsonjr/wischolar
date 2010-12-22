1522bd03c3c67683864d2af78b55d668231b8606
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2010-12-22 14:32:48 +0000

    clean up

--- sites/all/themes/openscholar/scholar_base/templates/maintenance-page.tpl.php
+++ sites/all/themes/openscholar/scholar_base/templates/maintenance-page.tpl.php
@@ -1,19 +1,3 @@
-<?php
-// $Id: maintenance-page.tpl.php,v 1.1.2.1 2009/02/13 18:17:08 johnalbin Exp $
-
-/**
- * @file maintenance-page.tpl.php
- *
- * Theme implementation to display a single Drupal page while off-line.
- *
- * All the available variables are mirrored in page.tpl.php. Some may be left
- * blank but they are provided for consistency.
- *
- *
- * @see template_preprocess()
- * @see template_preprocess_maintenance_page()
- */
-?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>">
 
@@ -23,99 +7,117 @@
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
+
 <body class="<?php print $body_classes; ?>">
-  <div id="page">
-    <div id="header">
+  <!--[if lte IE 6]><script src="/<?php print $directory;?>/js/ie6-warning/ie6-warning.js"></script> <script>window.onload=function(){e("/<?php print $directory;?>/js/ie6-warning/")}</script><![endif]-->
+    <?php if ($cp_toolbar) : ?>
+    <div id="top">
+  	   <?php print $cp_toolbar;?>
+  	 </div>
+  	 <?php endif;?>
+  <div id="wrapper">
+    <div id="wrapper-inner">
+      <div id="container">
 
-      <div id="skip-nav"><a href="#content"><?php print t('Skip to Main Content'); ?></a></div>
+      <?php if ($header_top || $header_main || $header_left || $header_right || $navbar) : ?>
 
-      <div id="logo-title">
-
-        <?php if (!empty($logo)): ?>
-          <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home">
-            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" id="logo" />
-          </a>
+      <div id="header">
+        <?php if (!empty($header_top)): ?>
+        <div id="header-top">
+          <?php print $header_top; ?>
+        </div><!-- /header-top -->
         <?php endif; ?>
-
-        <div id="name-and-slogan">
-
-        <?php if (!empty($site_name)): ?>
-          <div id="site-name"><strong>
-            <a href="<?php print $base_path; ?>" title="<?php print t('Home'); ?>" rel="home">
-              <?php print $site_name; ?>
-            </a>
-          </strong></div>
-        <?php endif; ?>
-
-        <?php if (!empty($site_slogan)): ?>
-          <div id="site-slogan">
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
-
-        </div> <!-- /name-and-slogan -->
-
-      </div> <!-- /logo-title -->
-
-      <?php if (!empty($header)): ?>
-        <div id="header-region">
-          <?php print $header; ?>
-        </div> <!-- /header-region -->
-      <?php endif; ?>
-
-    </div> <!-- /header -->
-
-    <div id="container" class="clearfix">
-
-      <?php if (!empty($left)): ?>
-        <div id="sidebar-left" class="column sidebar">
-          <?php print $left; ?>
-        </div> <!-- /sidebar-left -->
-      <?php endif; ?>
-
-      <div id="main" class="column"><div id="squeeze" class="clearfix">
-        <?php if (!empty($content_top)): ?>
-          <div id="content-top"><?php print $content_top; ?></div>
-        <?php endif; ?>
-        <div id="content">
-          <?php if (!empty($title)): ?>
-            <h1 class="title"><?php print $title; ?></h1>
+        <?php if ($header_main || $header_left || $header_right) : ?>
+        <div id="header-wrapper">
+          <?php if (!empty($header_main)): ?>
+            <div id="header-main" class="column">
+              <?php print $header_main; ?>
+            </div><!-- /header-main -->
           <?php endif; ?>
-          <?php print $messages; ?>
-          <?php print $content; ?>
-        </div> <!-- /content -->
-        <?php if (!empty($content_bottom)): ?>
-          <div id="content-bottom"><?php print $content_bottom; ?></div>
-        <?php endif; ?>
-      </div></div> <!-- /squeeze /main -->
 
-      <?php if (!empty($right)): ?>
-        <div id="sidebar-right" class="column sidebar">
-          <?php print $right; ?>
-        </div> <!-- /sidebar-right -->
+          <?php if (!empty($header_left)): ?>
+            <div id="header-left" class="column">
+              <?php print $header_left; ?>
+            </div><!-- /header-left -->
+          <?php endif; ?>
+
+          <?php if (!empty($header_right)): ?>
+          <div id="header-right" class="column">
+            <?php print $header_right; ?>
+          </div><!-- /header-right -->
+            <?php endif; ?>
+        </div><!-- /header wrapper -->
+        <?php endif; ?>
+        <?php if (!empty($navbar)): ?>
+        <div id="navbar">
+          <?php print $navbar; ?>
+        </div><!-- /navbar -->
+        <?php endif; ?>
+      </div> <!-- /header -->
       <?php endif; ?>
 
+      <div id="content-wrapper" class="clearfix">
+        <div id="content-wrapper-inner">
+          <div id="content-main" class="column">
+            <?php if (!empty($content_top)): ?>
+            <div id="content-top">
+              <?php print $content_top; ?>
+            </div><!-- /content-top -->
+            <?php endif; ?>
+
+            <div id="content">
+            <?php print $context_links;?>
+            <?php if (!empty($title)): ?>
+              <h2 class="title<?php if ($tabs) : print ' with-tabs'; endif;?>"><?php print $title; ?></h2>
+             <?php endif; ?>
+             <?php if (!empty($tabs)): ?>
+              <div class="tabs"><?php print $tabs; ?></div>
+              <?php endif; ?>
+              <?php print $help; ?>
+              <?php print $messages; ?>
+              <?php print $content; ?>
+            </div> <!-- /content -->
+
+            <?php if (!empty($content_bottom)): ?>
+              <div id="content-bottom">
+                <?php print $content_bottom; ?>
+              </div><!--/content-bottom-->
+            <?php endif; ?>
+            </div><!-- /content main -->
+
+              <?php if (!empty($left)): ?>
+          <div id="sidebar-first" class="sidebar column">
+            <?php print $left; ?>
+          </div> <!-- /sidebar-first -->
+          <?php endif; ?>
+
+          <?php if (!empty($right)): ?>
+          <div id="sidebar-second" class="sidebar column">
+            <?php print $right; ?>
+          </div> <!-- /sidebar-second -->
+          <?php endif; ?>
+        </div> <!-- / content wrapper inner -->
+      </div> <!-- / content wrapper -->
+      <div id="footer">
+        <div id="footer-inner">
+          <?php if ($footer) : ?>
+            <?php print $footer; ?>
+          <?php endif; ?>
+          <?php
+            global $user, $base_url;
+            $home_link =  l('Powered by OpenScholar','http://openscholar.harvard.edu', array('attributes' => array('class' => 'poweredby'),'html'=>TRUE));
+            $login_link = (!$user -> uid) ? l("Login", "user", array('purl' => array('disabled' => TRUE), 'attributes' => array('class' => 'footer-login'),'alias' => FALSE)) : "";
+          ?>
+          <p class="copy"><?php print $login_link;?> <?php if ($footer_message) { print $footer_message; } ?> <span id="powered-link"><?php print $home_link; ?></span></p><?php if(variable_get('openscholar_reportverinfo', 1)){ ?><img src="http://openscholar.harvard.edu/openscholar_lu/spacer.gif?<?php echo drupal_query_string_encode($openscholar_version_info) ?>" /><?php } ?>
+        </div><!-- /#footer-inner -->
+      </div> <!-- /#footer -->
     </div> <!-- /container -->
-
-    <?php if ($footer || $footer_message): ?>
-      <div id="footer-wrapper"><div id="footer">
-
-        <?php if ($footer_message): ?>
-          <div id="footer-message"><?php print $footer_message; ?></div>
-        <?php endif; ?>
-
-        <?php print $footer; ?>
-
-      </div></div> <!-- /#footer, /#footer-wrapper -->
-    <?php endif; ?>
-
-  </div> <!-- /page -->
-
+    </div><!-- /wrapper-inner -->
+  </div> <!-- /wrapper -->
+  <div id="extradiv"></div>
   <?php if ($closure_region): ?>
     <div id="closure-blocks"><?php print $closure_region; ?></div>
   <?php endif; ?>
-
   <?php print $closure; ?>
-
 </body>
 </html>
