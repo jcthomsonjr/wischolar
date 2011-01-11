0a5143d2d4e6cfd755f817e9863a2fa211c66b15
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2011-01-11 22:47:25 +0000

    update some publication form settings

--- sites/all/modules/openscholar_features/scholar_publications/theme/Drupal.scholarPublications.js
+++ sites/all/modules/openscholar_features/scholar_publications/theme/Drupal.scholarPublications.js
@@ -1,46 +1,24 @@
 
-
-/**
- * Generic replacement click handler to open the modal with the destination
- * specified by the href of the link.
- */
-var clickAJAXPublication = function() {
-  if ($(this).hasClass('ctools-ajaxing')) {
-    return false;
-  }
-
-  var url = "/publications/abstract";
-  var object = $(this);
-  $(this).addClass('ctools-ajaxing');
-  try {
-    url = url.replace('/nojs/', '/ajax/');
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: 'path='+$(this).attr('href'),
-      global: true,
-      success: Drupal.CTools.AJAX.respond,
-      error: function() { 
-        alert("An error occurred while attempting to process " + url); 
-      },
-      complete: function() {
-        object.removeClass('ctools-ajaxing');
-      },
-      dataType: 'json'
-    });
-  }
-  catch (err) {
-    alert("An error occurred while attempting to process " + url); 
-    $(this).removeClass('ctools-ajaxing');
-    return false;
-  }
-
-  return false;
+Drupal.behaviors.scholarPublications = function() {
+	
+	if($('div#biblio-authors-wrapper').length){
+		scholarPublicationsMonitorAuthors();
+	}
 };
 
-Drupal.behaviors.scholarlayout = function() {
-	  //Tell Ctools to take over the click for the abstract	
-	$('.'+Drupal.settings.scholarPublications.abstract_id+' a:not(.publication-abstract-processed)')
-    .addClass('publication-abstract-processed')
-    .click(clickAJAXPublication);
+scholarPublicationsMonitorAuthors  = function() {
+  
+  $('div#biblio-authors-wrapper input:text:last').change(function(){
+    if($(this).val() && !$(this).hasClass('no-more-authors') && !$(this).next('div#biblio-authors-wrapper input:text').length){
+      $(this).addClass('no-more-authors');
+      window.setTimeout('$("input#edit-contributors1-wrapper-biblio-authors-more").mousedown();', 200);
+	}
+  });
+  
+  $('div#biblio-secondary-authors-wrapper input:text:last').change(function(){
+    if($(this).val() && !$(this).hasClass('no-more-authors') && !$(this).next('div#biblio-secondary-authors-wrapper input:text').length){
+      $(this).addClass('no-more-authors');
+      window.setTimeout('$("input#edit-contributors2-wrapper-biblio-secondary-authors-more").mousedown();', 200);
+    }
+  });
 };
