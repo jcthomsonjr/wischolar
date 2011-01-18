bd713a16c7c2862d816dc77bb1dd8d3f5800cae3
rwbrandon <rwbrandon@7b190466-e17b-4636-afb0-0a889111a0fc>
2011-01-18 20:18:47 +0000

    change login link to bring you back to where you were

--- sites/all/themes/custom/gking/page.tpl.php
+++ sites/all/themes/custom/gking/page.tpl.php
@@ -114,9 +114,8 @@
             <?php print $footer; ?>
           <?php endif; ?>
           <?php
-            global $user, $base_url;
             $home_link =  l('Powered by OpenScholar','http://openscholar.harvard.edu', array('attributes' => array('class' => 'poweredby'),'html'=>TRUE));
-            $login_link = (!$user -> uid) ? l("Login", "user", array('purl' => array('disabled' => TRUE), 'attributes' => array('class' => 'footer-login'),'alias' => FALSE)) : "";
+            $login_link = theme('vsite_login_link',"Login",array('class' => 'footer-login'));
           ?>
           <p class="copy"><?php print $login_link;?> <?php if ($footer_message) { print $footer_message; } ?> <span id="powered-link"><?php print $home_link; ?></span></p><?php if(variable_get('openscholar_reportverinfo', 1)){ ?><img src="http://openscholar.harvard.edu/openscholar_lu/spacer.gif?<?php echo drupal_query_string_encode($openscholar_version_info) ?>" /><?php } ?>
         </div><!-- /#footer-inner -->
