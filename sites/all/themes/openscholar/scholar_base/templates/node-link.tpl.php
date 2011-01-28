3675d0c1dfce9d1d445f079e1de73ee3f5f5ff5e
swortis <swortis@7b190466-e17b-4636-afb0-0a889111a0fc>
2011-01-28 21:26:15 +0000

    changes to forms within CP and other tweaks

---
+++ sites/all/themes/openscholar/scholar_base/templates/node-link.tpl.php
@@ -0,0 +1,35 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $node_classes; ?>">
+  <div class="node-inner">
+    <?php print $picture; ?>
+    <?php if (!$page): ?>
+      <div class="os-links">
+        <?php print $links; ?>
+      </div>
+      <h3 class="title">
+        <a href="<?php print $node_url; ?>" title="<?php print $title ?>"><?php print $title; ?></a>
+      </h3>
+    <?php endif; ?>
+    <?php if ($unpublished): ?>
+      <div class="unpublished"><?php print t('Unpublished'); ?></div>
+    <?php endif; ?>
+    <?php if ($submitted): ?>
+        <p class="submitted"><?php print $submitted; ?></p>
+      <?php endif; ?>
+     <?php if ($page): ?>
+     <?php if ($terms): ?>
+       <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+     <?php endif; ?>
+     <?php endif; ?>
+    <div class="content">
+      <?php print $content; ?>
+    </div>
+    <?php if (!$page): ?>
+      <?php if ($terms): ?>
+      <div class="terms terms-inline"><?php print t(' in ') . $terms; ?></div>
+      <?php endif; ?>
+    <?php endif; ?>
+    <?php if ($page) {
+      print $links;
+      } ?>
+  </div> <!-- /node-inner -->
+</div> <!-- /node -->
