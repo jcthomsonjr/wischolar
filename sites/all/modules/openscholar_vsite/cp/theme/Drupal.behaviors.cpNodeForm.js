3a298f1e4977219ebcfe6b521eaa59118a0dde81
Richard Brandon <richard.brandon@gmail.com>
2011-03-22 15:09:35 -0400

    add opensholar_vsite files

---
+++ sites/all/modules/openscholar_vsite/cp/theme/Drupal.behaviors.cpNodeForm.js
@@ -0,0 +1,8 @@
+Drupal.behaviors.cpNodeForm = function() {
+
+	if($('form#node-form fieldset.vt-advanced-options').length < 1){
+	  $('form#node-form div.vertical-tabs').wrap('<fieldset class="vt-advanced-options collapsible collapsed" />');
+	  $('form#node-form fieldset.vt-advanced-options').prepend('<legend>Advanced Options</legend>')
+	}
+	
+};
