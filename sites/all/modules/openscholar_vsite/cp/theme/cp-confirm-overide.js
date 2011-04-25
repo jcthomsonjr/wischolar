7605e569e319d6e3f767062334fe47c4fd6f8e4a
MPetrovic <mpetrovic@iq.harvard.edu>
2011-04-25 16:33:52 -0400

    Migrating shield settings from vsite_design to vsite_shield.

--- sites/all/modules/openscholar_vsite/cp/theme/cp-confirm-overide.js
+++ sites/all/modules/openscholar_vsite/cp/theme/cp-confirm-overide.js
@@ -1,8 +1,16 @@
 Drupal.behaviors.cp_confirm_overide_handler = function (context) {
 		var s_urls = "";
-		var s_container = $("div#cp-content").length ? "div#cp-content":"div.content";
-		$(s_container+" a:not(.cp_confirm_overide_handler-processed)").addClass('cp_confirm_overide_handler-processed').each(function(){
-			s_urls += "paths[]="+$(this).attr('href')+"&";
+		var s_container = $("div#cp-content").length ? "div#cp-content":"div#container";
+		var links = $(s_container+" a:not(.cp_confirm_overide_handler-processed)");
+		links.addClass('cp_confirm_overide_handler-processed');
+		links.each(function(){
+			var url = $(this).attr('href');
+			if (typeof (url) == 'string') {
+			  if (url.indexOf('/') == 0) {
+				url = url.replace(Drupal.settings.basePath, '');
+			  }
+			  s_urls += "paths[]="+url+"&";
+			}
 		});
 
 		//Make call to check the urls to replace
@@ -19,12 +27,17 @@ Drupal.behaviors.cp_confirm_overide_handler = function (context) {
 var cp_overide_add_click = function(return_value){
 	cp_override_processing_ajax = false;
 
-	var s_container = $("div#cp-content").length ? "div#cp-content":"div.content";
-	for ( href in return_value.overideable_settings) {
+	var s_container = $("div#cp-content").length ? "div#cp-content":"div#container";
+	for (var href in return_value.overideable_settings) {
 		if(return_value.overideable_settings[href]){
 			cp_add_dialog();
 
-			$(s_container+" a[href^=\""+href.replace(/([#;&,\.\+\*\~':"\!\^$\[\]\(\)=>\|])/g, "\\$1")+"\"]").click(function(event){
+			if (href.indexOf('http:') == -1) {
+				href = Drupal.settings.basePath + href;
+			}
+			href = href.replace(href.substr(href.indexOf('?')), '');
+			var selector = s_container+' a[href*='+href.replace(/([#;&,\.\+\*\~':"\!\^$\[\]\(\)=>\|])/g, "\\$1")+']';
+			$(selector).click(function(event){
 			  if(!$('#cp_confirm_dialog').dialog('isOpen')){
 				  var link = $(this);
 				  $('#cp_confirm_dialog span').html(link.html()+". Are you sure?");
@@ -79,12 +92,14 @@ var cp_override_processing_ajax = false;
 var cp_override_url_check = function(s_urls){
 	if(cp_override_processing_ajax) return;
 	cp_override_processing_ajax = true;
+	var vsite = Drupal.settings.og.group_context.title;
+	var url = 'http://'+location.host+Drupal.settings.basePath+vsite+'/cp/allow_callback_override';
 	$.ajax({
 	   type: "POST",
-	   url: location.pathname.substring(0,location.pathname.indexOf('/',1))+"/cp/allow_callback_override",
+	   url: url,
 	   dataType: 'json',
 	   data: s_urls+"js=1",
-	   success: cp_overide_add_click,
+	   success: cp_overide_add_click
     });
 };
 
