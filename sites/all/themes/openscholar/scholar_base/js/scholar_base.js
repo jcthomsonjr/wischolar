f77f6b7a5bc97ebb73071a96b4989fcae8b79bb7
Shaun Wortis <swortis@sandbox.hmdc.harvard.edu>
2011-05-12 15:58:37 -0400

    themes fixes

--- sites/all/themes/openscholar/scholar_base/js/scholar_base.js
+++ sites/all/themes/openscholar/scholar_base/js/scholar_base.js
@@ -1,16 +1,14 @@
-(function ($) {
-$.fn.vAlign = function() {
+$.fn.vAlign = function(offset) {
 	return this.each(function(i){
 	var ah = $(this).height();
 	var ph = $(this).parents('.wrap').height();
-	var mh = Math.ceil((ph-ah) / 2) - 10;
+	var mh = Math.ceil((ph-ah) / 2) - offset;
 	$(this).css('margin-top', mh);
 	});
 };
-})(jQuery);
 
 $(document).ready(function() {
-  $('#header #block-scholar-1').vAlign();
-  $('#header #block-scholar_project-1').vAlign();
-  $('#header #scholar-shield').vAlign();
+  $('#header #block-scholar-1').vAlign(10);
+  $('#header #block-scholar_project-1').vAlign(10);
+  $('#header #scholar-shield').vAlign(20);
 });
