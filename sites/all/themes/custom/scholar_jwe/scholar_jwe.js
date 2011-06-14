32db5f169f13272a3b10e9cd526ab0ab74373d4b
MPetrovic <mpetrovic@iq.harvard.edu>
2011-06-14 15:29:42 -0400

    Fix for left column being out of place in jwe custom theme.

--- sites/all/themes/custom/scholar_jwe/scholar_jwe.js
+++ sites/all/themes/custom/scholar_jwe/scholar_jwe.js
@@ -4,6 +4,14 @@ var images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg'];
 $(document).ready(function() {
     $('.page-home #content-main').prepend('<div id="front-rotate"></div>');
     $('#front-rotate').css({'background-image': 'url(/sites/all/themes/custom/scholar_jwe/images/rotating/' + images[Math.floor(Math.random() * images.length)] + ')'});
+    
+    /*
+    var offset = document.all['content-main'].offsetWidth * -1;
+    offset += parseFloat(document.all['content-main'].currentStyle.paddingLeft);
+    offset += parseFloat(document.all['sidebar-first'].currentStyle.paddingLeft);
+    offset += parseFloat(document.all['sidebar-first'].currentStyle.paddingRight);
+    $('#sidebar-first').css({'margin-left': offset+'px'});
+    */
   });
 
 //$('#front-rotate').css({'background-image': 'url(/sites/all/themes/custom/scholar_jwe/images/rotating/' + images[Math.floor(Math.random() * images.length)] + ')'});
