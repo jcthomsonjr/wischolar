2b60d9fb7329af22e4f8af7a2169754a00792c38
MPetrovic <mpetrovic@iq.harvard.edu>
2011-06-15 11:33:51 -0400

    CSS and js fixes for jwe custom theme.

--- sites/all/themes/custom/scholar_jwe/scholar_jwe.js
+++ sites/all/themes/custom/scholar_jwe/scholar_jwe.js
@@ -2,8 +2,8 @@ var pathname = window.location.pathname;
 var images = ['01.jpg', '02.jpg', '03.jpg', '04.jpg'];
 
 $(document).ready(function() {
-    $('.page-home #content-main').prepend('<div id="front-rotate"></div>');
-    $('#front-rotate').css({'background-image': 'url(/sites/all/themes/custom/scholar_jwe/images/rotating/' + images[Math.floor(Math.random() * images.length)] + ')'});
+    $('.front #content-main').prepend('<div id="front-rotate"></div>');
+    $('#front-rotate').css({'background-image': 'url(sites/all/themes/custom/scholar_jwe/images/rotating/' + images[Math.floor(Math.random() * images.length)] + ')'});
     
     /*
     var offset = document.all['content-main'].offsetWidth * -1;
