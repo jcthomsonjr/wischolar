2ae1903c0352c84d53416c7ff7089b5b70e42a2f
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2011-06-17 17:10:23 -0400

    Added skeleton of new citation_distribute module.  Shows up in advanced options, but doesn't do anything yet.

---
+++ sites/all/modules/openscholar_scholar/citation_distribute/citation_distribute.js
@@ -0,0 +1,16 @@
+// The following line will prevent a JavaScript error if this file is included and vertical tabs are disabled.
+Drupal.verticalTabs = Drupal.verticalTabs || {};
+
+Drupal.verticalTabs.citation_distribute_selections = function() {
+  var vals = [];
+  
+  /* Get checked options, add their label text to array */
+  $('fieldset.vertical-tabs-citation_distribute_selections input:checked')
+    .parent()
+    .each( function(){vals.push( $(this).text() )} )
+  
+  if (vals.join(', ') == '') {
+    return Drupal.t('None');
+  }
+  return vals.join(', ');
+}
