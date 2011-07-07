a073c2f7722be3c3a70f43411d91c7574e54dd15
MPetrovic <mpetrovic@iq.harvard.edu>
2011-07-07 13:15:54 -0400

    Adding wysiwyg_fields and os_wysiwyg's js to make them work better.

---
+++ sites/all/modules/os/modules/os_wysiwyg/theme/os_wysiwyg.js
@@ -0,0 +1,34 @@
+/**
+ * Overrides some of wysiwyg_fields' functions to be more intuitive
+ */
+(function ($) {
+	if (typeof Drupal.wysiwyg == 'undefined')
+		return;
+	
+	function cleanUp(id) {
+		// remove the 'Expand' button. There's nothing hidden to Expand.
+		$('.ui-dialog-titlebar .wysiwyg_fields-icon-expand').remove();
+		
+		// remove the Insert button added by wysiwyg_fields.
+		// The button provided by Insert is more useful.
+		$('.ui-dialog-buttonpane').remove();
+		
+		// Show the table
+		$('#' + id.replace('_', '-', 'g') + '-items, #wysiwyg_fields-' + id + '-wrapper table').show();
+	}
+	
+	// store the current functions
+	var dialogShowDefaultOld = Drupal.wysiwygFields.dialogShowDefault,
+		dialogShowUpdateOld = Drupal.wysiwygFields.dialogShowUpdate;
+	
+	// overwrite current functions with new ones that call the current funcs and tweak the results.
+	Drupal.wysiwygFields.dialogShowDefault = function (id) {
+		dialogShowDefaultOld.call(Drupal.wysiwygFields, id);
+		cleanUp(id);
+	}
+	
+	Drupal.wysiwygFields.dialogShowUpdate = function (id) {
+		dialogShowDefaultUpdate.call(Drupal.wysiwygFields, id);
+		cleanUp(id);
+	}
+})(jQuery);
