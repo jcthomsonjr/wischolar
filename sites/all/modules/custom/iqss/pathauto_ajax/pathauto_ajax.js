9e8f3bcd78f0388e54b42c4373d7e46301334211
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2011-07-12 16:56:47 -0400

    Bash to jquery for title replacement.  Still unfinished.

--- sites/all/modules/custom/iqss/pathauto_ajax/pathauto_ajax.js
+++ sites/all/modules/custom/iqss/pathauto_ajax/pathauto_ajax.js
@@ -6,11 +6,13 @@
 
 /* attach a click even to the path tab */
 $(document).ready( function() {
-	$('a.vertical-tabs-list-path').click(function(){
-		if ($('#edit-path').attr('value') == '') {
+	//$('a.vertical-tabs-list-path').click(function(){
+	$('#edit-title').live( 'click', (function() {  //focusout isn't supported yet.  jq 1.4.1
+		if ( $('#edit-title').attr('value') != '') {
+			alert('its alive');
 			make_alias();
 		}
-	});
+	}));
 });
 
 /**
@@ -19,10 +21,15 @@ $(document).ready( function() {
  * on success, replace the path with the results
  */
 function make_alias() {
-	data = {};
-	data.type =  document.URL.split('/').pop().split('?').shift();
+	data = Drupal.settings.pathauto_ajax;
+//	data.type =  document.URL.split('/').pop().split('?').shift();
+	//data.URL = document.URL;
+	//data.space_og_path = document.URL.split('/')[3];
+	//data.get = window.location.search;
+	
+	//data.space_og_path = Drupal.settings.og.group_context.title;
+
 	data.title_raw = $('#edit-title').attr('value');
-	data.space_og_path = document.URL.split('/')[3];
 	
 	href = 'http://' + document.location.host + '/pathauto_ajax/alias_js';
 	$.getJSON(href, data, function(json) {
