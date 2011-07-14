a61a55954bd00a1ac2aea4398c8d739d8f3a0071
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2011-07-14 15:24:41 -0400

    aliases all urls.  no more approved pattern list.  cleaned .js file.

--- sites/all/modules/custom/iqss/pathauto_ajax/pathauto_ajax.js
+++ sites/all/modules/custom/iqss/pathauto_ajax/pathauto_ajax.js
@@ -12,31 +12,21 @@
 Drupal.behaviors.pathauto_ajax = function(context, settings) {
 	$('#edit-title').bind('blur', function() {
 		if ($('#edit-title').attr('value') != '') {
-			make_alias();
+			
+		  //collect some data to pass back to pathauto_ajax_alias_js
+		  var data = Drupal.settings.pathauto_ajax;
+		  data.title = $.trim($('#edit-title').attr('value'));
+
+		  //send data by ajax.  callback places result in #edit-path's value attribute
+		  var href = 'http://' + document.location.host + '/pathauto_ajax/alias_js';
+		  $.getJSON(href, data, function(json) {
+		    if (json.status && json.data.length > 0) {
+		      $('#edit-path').attr('value', json.data);
+		    }
+		  })
+		  
 		}
 	});
 };
 
 
-/**
- * ajax call back to drupal pass it some information from the current page,
- * since that can't be loaded from the node yet on success, replace the path
- * with the results
- */
-function make_alias() {
-	var data = Drupal.settings.pathauto_ajax;
-	data.title = $.trim($('#edit-title').attr('value'));
-
-	var now = new Date();
-	data.yyyy = now.getFullYear();
-	data.mm = "" + (now.getMonth() + 1);
-	if (data.mm.length == 1)
-		data.mm = "0" + data.mm.toString();
-
-	var href = 'http://' + document.location.host + '/pathauto_ajax/alias_js';
-	$.getJSON(href, data, function(json) {
-		if (json.status && json.data.length > 0) {
-			$('#edit-path').attr('value', json.data);
-		}
-	})
-}
