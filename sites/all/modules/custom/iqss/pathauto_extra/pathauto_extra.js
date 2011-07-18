53e24baef426f4360292699259516941c6b9e72b
Jon Sagotsky <jsagotsky@iq.harvard.edu>
2011-07-18 15:25:00 -0400

    Renamed pathauto_ajax to pathauto_extra.

--- sites/all/modules/custom/iqss/pathauto_extra/pathauto_extra.js
+++ sites/all/modules/custom/iqss/pathauto_extra/pathauto_extra.js
@@ -1,5 +1,5 @@
 /**
- * pathauto_ajax.js
+ * pathauto_extra.js
  * 
  * retrieves path of new node from drupal, inserts it into pathauto form
  */
@@ -9,16 +9,16 @@
  * event binding instead.
  */
 
-Drupal.behaviors.pathauto_ajax = function(context, settings) {
+Drupal.behaviors.pathauto_extra = function(context, settings) {
 	$('#edit-title').bind('blur', function() {
 		if ($('#edit-title').attr('value') != '') {
 			
-		  //collect some data to pass back to pathauto_ajax_alias_js
-		  var data = Drupal.settings.pathauto_ajax;
+		  //collect some data to pass back to pathauto_extra_alias_js
+		  var data = Drupal.settings.pathauto_extra;
 		  data.title = $.trim($('#edit-title').attr('value'));
 
 		  //send data by ajax.  callback places result in #edit-path's value attribute
-		  var href = 'http://' + document.location.host + '/pathauto_ajax/alias_js';
+		  var href = 'http://' + document.location.host + '/pathauto_extra/alias_js';
 		  $.getJSON(href, data, function(json) {
 		    if (json.status && json.data.length > 0) {
 		      $('#edit-path').attr('value', json.data);
