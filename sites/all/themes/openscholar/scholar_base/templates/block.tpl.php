2e0239f7e5dbc452597f4df26ea239c16cc9150f
MPetrovic <mpetrovic@iq.harvard.edu>
2011-08-12 11:19:25 -0400

    Don't printout admin links if there are none to print out.

--- sites/all/themes/openscholar/scholar_base/templates/block.tpl.php
+++ sites/all/themes/openscholar/scholar_base/templates/block.tpl.php
@@ -3,10 +3,11 @@
   <?php if ($block->subject): ?>
     <h3 class="title"><?php print $block->subject; ?></h3>
   <?php endif; ?>
-
+  <?php if ($vsite_admin_links):?>
   <div class="os-links">
     <?php print $vsite_admin_links; ?>
   </div>
+  <?php endif; ?>
   <div class="content">
     <?php print $block->content; ?>
   </div>
